159b774cc0fe71f948c1fbf030bae54fed131053
pierre schuvey <chromatykretro@gmail.com>
2023-02-27 18:58:39 +0100

    assets

--- src/component/pkmList.js
+++ src/component/pkmList.js
@@ -1,21 +1,9 @@
 import React,{useState, useEffect} from 'react';
 import Axios from 'axios'
 import '../App.css'
-import PkmList from './pkmList.js'
 
-function HomePage() {
-
-const [pseudo,setPseudo] = useState("");
-const [list,setList] = useState([]);
-const submitPost = () => {
-  setList(Axios.get(`https://chromatyk-pokemon.herokuapp.com/api/getByUser/${pseudo}`))
-}
-
+function PkmList(props) {
     return (
-      <>
-        <div>
-        <PkmList list={list}/>
-        </div>
         <div className="CreatePost">
             <div className="uploadPost">
                 <label>Pseudo: </label>
@@ -25,7 +13,6 @@ const submitPost = () => {
                 <button onClick={submitPost}>Submit Post</button>
             </div>
         </div>
-      </>
     )
 }
 
