41aadbe43b9684e574153ec5cbd2eadd8c8ac7d4
Chromatyk2 <126329962+Chromatyk2@users.noreply.github.com>
2023-02-28 11:53:02 +0100

    Update pkmList.js

--- src/component/pkmList.js
+++ src/component/pkmList.js
@@ -5,13 +5,13 @@ import '../App.css'
 function PkmList(props) {
   const pkmList = props.list;
     return (
-      <div className="pokemonGlobalContainer">
+      <div className="pokemonGlobalContainer row">
            {pkmList == [] ? (
              <h1>Loading...</h1>
            ) : (
                pkmList.map((val, key) => {
                  return (
-                   <div className="uniquePokemonContainer">
+                   <div className="col-3 uniquePokemonContainer">
                       <img src={val.pkmImage}></img>
                   </div>
                  )
