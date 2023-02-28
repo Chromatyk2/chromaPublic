f7f8ebc26da39e7f484f3cf1c57e55c0923032f6
pierre schuvey <chromatykretro@gmail.com>
2023-02-28 19:01:28 +0100

    enfin

--- src/component/pkmList.js
+++ src/component/pkmList.js
@@ -1,20 +1,37 @@
 import React,{useState, useEffect} from 'react';
 import ReactPaginate from 'react-paginate';
 import Axios from 'axios'
+import Pagination from '../Pagination';
 import '../App.css'
 
 function PkmList(props) {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+  
   const pkmList = props.list;
   const shinys = pkmList.filter(item => item.shiny == 1);
   const nbShiny = shinys.length;
   const nbTotal = pkmList.length;
     return (
       <>
+
         <div className="stats">
           <p className="labelStats">Shiny : <span className="valueStats">{nbShiny}</span></p>
           <p className="labelStats">Total : <span className="valueStats">{nbTotal}</span></p>
         </div>
         <div className="pokemonGlobalContainer">
+          <Pagination
+            className="pagination-bar"
+            currentPage={currentPage}
+            totalCount={props.list.length}
+            pageSize={PageSize}
+            onPageChange={page => setCurrentPage(page)}
+          />
              {pkmList == [] ? (
                <h1>Loading...</h1>
              ) : (
@@ -35,43 +52,5 @@ function PkmList(props) {
      );
 }
 
-function PaginatedItems({ itemsPerPage }) {
-  // Here we use item offsets; we could also use page offsets
-  // following the API or data you're working with.
-  const [itemOffset, setItemOffset] = useState(0);
 
-  // Simulate fetching items from another resources.
-  // (This could be items from props; or items loaded in a local state
-  // from an API endpoint with useEffect and useState)
-  const endOffset = itemOffset + itemsPerPage;
-  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-  const currentItems = items.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(props.list.length / itemsPerPage);
-
-  // Invoke when user click to request another page.
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % props.list.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
-    setItemOffset(newOffset);
-  };
-
-  return (
-    <>
-      <Items currentItems={currentItems} />
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
-      />
-    </>
-  );
-}
-
-
-export default <PaginatedItems itemsPerPage={25} />
+export default PkmList
