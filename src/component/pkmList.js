28c22a32ee1ffe1ceff6607de663bb6e8a775079
Chromatyk2 <126329962+Chromatyk2@users.noreply.github.com>
2023-02-28 12:10:22 +0100

    Update pkmList.js

--- src/component/pkmList.js
+++ src/component/pkmList.js
@@ -4,7 +4,7 @@ import '../App.css'
 
 function PkmList(props) {
   const pkmList = props.list;
-  const shinys = data.filter(item => item.shiny === 1));
+  const shinys = data.filter(item => item.shiny == 1);
   const nbShiny = shiny.length;
     return (
       <div className="pokemonGlobalContainer">
