ec3855d875662fa2dabfedbeb0522713c937358a
Chromatyk2 <126329962+Chromatyk2@users.noreply.github.com>
2023-03-01 14:25:44 +0100

    Update paginate.js

--- src/component/paginate.js
+++ src/component/paginate.js
@@ -14,13 +14,15 @@ function Items(props) {
        ) : (
            pkmList.map((val, key) => {
              return (
-               <div className="uniquePokemonContainer">
-                 <div className="infoPkm">
-                   {val.nbCapture > 1 ? <div className="infoNbCapture">{val.nbCapture}</div> : <div></div>}
-                   {val.shiny == 1 ? <img className="infoShiny" src="https://www.depaul.org/wp-content/uploads/2022/02/DePaul-Shining-Star-Program-Blue-Icon.png"></img> : <div></div>}
-                 </div>
-                  <img src={val.pkmImage}></img>
-              </div>
+               <>
+                 <div className="uniquePokemonContainer">
+                   <div className="infoPkm">
+                     {val.nbCapture > 1 ? <div className="infoNbCapture">{val.nbCapture}</div> : <div></div>}
+                     {val.shiny == 1 ? <img className="infoShiny" src="https://www.depaul.org/wp-content/uploads/2022/02/DePaul-Shining-Star-Program-Blue-Icon.png"></img> : <div></div>}
+                   </div>
+                    <img src={val.pkmImage}></img>
+                </div>
+              </>
              )
            })
        )}
