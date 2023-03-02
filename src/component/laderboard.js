259a3a5a4b269c8e177e4104ba013d1aceff0ddd
PierreSchuvey <chromatykretro@gmail.com>
2023-03-02 17:02:18 +0100

    oui

--- src/component/laderboard.js
+++ src/component/laderboard.js
@@ -82,7 +82,7 @@ function LaderBoard() {
           others.map((val, key) => {
           return (
             <div class="item">
-              <div class="pic">#{key}</div>
+              <div class="pic"><p>#{key}</p></div>
               <div class="name">
                 {val.pseudo}
               </div>
