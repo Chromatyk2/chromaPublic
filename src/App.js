b81082faf78c5bd0a9733df1a927e20f55a6403f
PierreSchuvey <chromatykretro@gmail.com>
2023-03-02 12:35:36 +0100

    yesy

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@
 import $ from "jquery";
 import './App.css';
 import HomePage from './component/home.js';
+import BasicExample from './component/nav.js';
 
 function App() {
   return(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BasicExample />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
