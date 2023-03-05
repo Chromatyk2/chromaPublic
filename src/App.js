1a7db0edcd2c9c42d175e2c49882de04f7716ef7
pierre schuvey <chromatykretro@gmail.com>
2023-03-05 13:14:13 +0100

    tooltip

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import 'react-tooltip/dist/react-tooltip.css'
 import HomePage from './component/home.js';
 import BasicExample from './component/nav.js';
 import LaderBoard from './component/laderboard.js';
-
+import { Link } from "react-router-dom";
 function App() {
   return(
     <>
+      <Link to={`/`}>Pokédex</Link>
+      <Link to={`/leaderboard`}>Classement</Link>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
