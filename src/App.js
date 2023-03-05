2748ce97267f6bd262f7d67c53d60dc804b6c8e1
pierre schuvey <chromatykretro@gmail.com>
2023-03-05 23:44:40 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
- import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import $ from "jquery";
 import './App.css';
 import 'react-tooltip/dist/react-tooltip.css'
 import HomePage from './component/home.js';
 import BasicExample from './component/nav.js';
 import LaderBoard from './component/laderboard.js';
+import NavBar from './component/navbar.js';
 function App() {
   return(
     <>
