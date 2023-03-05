a37bf2048fbb0680550bb552ff96dae1c74da79b
pierre schuvey <chromatykretro@gmail.com>
2023-03-05 13:11:13 +0100

    tooltip

--- src/component/paginate.js
+++ src/component/paginate.js
@@ -17,7 +17,7 @@ function Items(props) {
            pkmList.map((val, key) => {
              return (
                <>
-                 <div className="anchorTooltip uniquePokemonContainer" data-tooltip-content={moment(val.dateCapture).utc().format('DD/MM/YYYY')}>
+                 <div className="anchorTooltip uniquePokemonContainer" data-tooltip-content={val.pkmName+" - "+moment(val.dateCapture).utc().format('DD/MM/YYYY')}>
                    <div className="infoPkm">
                      {val.nbCapture > 1 ? <div className="infoNbCapture">{val.nbCapture}</div> : <div></div>}
                      {val.shiny == 1 ? <img className="infoShiny" src="https://www.depaul.org/wp-content/uploads/2022/02/DePaul-Shining-Star-Program-Blue-Icon.png"></img> : <div></div>}
