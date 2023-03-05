8faf8c7b34ab38738bcc6631bb7f5d25bf035ff2
pierre schuvey <chromatykretro@gmail.com>
2023-03-05 13:08:49 +0100

    tooltip

--- src/component/paginate.js
+++ src/component/paginate.js
@@ -1,14 +1,13 @@
 import React,{useState, useEffect} from 'react';
 import ReactPaginate from 'react-paginate';
 import { Tooltip } from 'react-tooltip'
-import Moment from 'moment';
+import moment from 'moment';
 
 function Items(props) {
   const pkmList = props.currentItems;
   const shinys = pkmList.filter(item => item.shiny == 1);
   const nbShiny = shinys.length;
   const nbTotal = pkmList.length;
-  Moment.locale('fr');
   return (
     <>
       <div className="pokemonGlobalContainer">
@@ -18,7 +17,7 @@ function Items(props) {
            pkmList.map((val, key) => {
              return (
                <>
-                 <div className="anchorTooltip uniquePokemonContainer" data-tooltip-content={val.dateCapture.toLocaleString()}>
+                 <div className="anchorTooltip uniquePokemonContainer" data-tooltip-content={moment(val.dateCapture).utc().format('YYYY-MM-DD')}>
                    <div className="infoPkm">
                      {val.nbCapture > 1 ? <div className="infoNbCapture">{val.nbCapture}</div> : <div></div>}
                      {val.shiny == 1 ? <img className="infoShiny" src="https://www.depaul.org/wp-content/uploads/2022/02/DePaul-Shining-Star-Program-Blue-Icon.png"></img> : <div></div>}
