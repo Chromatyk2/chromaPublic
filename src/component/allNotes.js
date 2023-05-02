02a828633a08143c071c76cf2331bb00ee20000d
pierre schuvey <chromatykretro@gmail.com>
2023-05-02 20:36:43 +0200

    fix

--- src/component/allNotes.js
+++ src/component/allNotes.js
@@ -35,7 +35,7 @@ function AllNotes(props) {
     if (myNote !== null){
       return (
         <>
-          <div style={{boxShadow:"inset 0 0 "+loading*4+"px 0 #0f0, inset 0 0 "+loading*8+"px 0 #f0f",boxShadow:"0 0 "+loading*2+"px 0 #0f0, 0 0 "+loading*4+"px 0 #f0f"}} class="c-progress-circle" id="circle" data-percentage={loading}>
+          <div style={{boxShadow:"inset 0 0 "+loading*4+"px 0 #0f0, inset 0 0 "+loading*8+"px 0 #f0f, 0 0 "+loading*2+"px 0 #0f0, 0 0 "+loading*4+"px 0 #f0f"}} class="c-progress-circle" id="circle" data-percentage={loading}>
             <svg class="c-progress-circle__svg">
               <defs>
                 <linearGradient id="gradient">
