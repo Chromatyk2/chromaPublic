4f76cb0fb38449c31ead8f84f4a49ed47f9931f8
pierre schuvey <chromatykretro@gmail.com>
2023-05-02 18:50:54 +0200

    fix

--- src/component/allNotes.js
+++ src/component/allNotes.js
@@ -21,13 +21,13 @@ function AllNotes(props) {
       })
     }, [])
     useEffect(() => {
-        const interval = setInterval(() => {
-          if(loading < myNote){
-            console.log(loading);
-            console.log(myNote);
-            setLoading((loading) => loading + 1);
-          }
-        }, 100);
+      const interval = setInterval(() => {
+        if(loading > myNote){
+          setLoading((loading) => loading + 1);
+        }else(
+          clearInterval(interval);
+        )
+      }, 1000);
     }, []);
     if (myNote !== null){
       return (
