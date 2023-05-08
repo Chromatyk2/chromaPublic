5f5b0f92592beb4a688889f041b3a043610de6ec
pierre schuvey <chromatykretro@gmail.com>
2023-05-08 07:44:42 +0200

    banger

--- src/component/viewersNote.js
+++ src/component/viewersNote.js
@@ -22,18 +22,18 @@ function ViewersNote(props) {
             sum+=val.note;
           });
           setMyNote(Math.round(sum/response.data.length));
-          if( myNote !== null){
-            if(myNote < 7){
-              setColor("red");
-            }
-            if(myNote > 14){
-              setColor("green");
-            }else{
-              setColor("orange");
-            }
-          }
         })
     }, [])
+    if( myNote !== null){
+      if(myNote < 7){
+        setColor("red");
+      }
+      if(myNote > 14){
+        setColor("green");
+      }else{
+        setColor("orange");
+      }
+    }
     if (myNote !== null){
       return (
         <>
