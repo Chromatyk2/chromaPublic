d5af82149e9697cb27632da1b1652caaf01a1c13
pierre schuvey <chromatykretro@gmail.com>
2023-05-08 20:34:34 +0200

    fix

--- src/component/nostalPick.js
+++ src/component/nostalPick.js
@@ -10,7 +10,7 @@ import moment from 'moment';
 function NostalPick(props) {
   return(
     <div id="odds">
-      <p style={{fontWeight:"bold",textAlign:"center""}}>Taux :</p>
+      <p style={{fontWeight:"bold",textAlign:"center"}}>Taux :</p>
       <p style={{fontWeight:"bold",color:"green"}}>Rare : 75% (1 Pokemon Random)</p>
       <p style={{fontWeight:"bold",color:"purple"}}>Epic : 25% (1 Pokemon Taux Shiny x 2)</p>
       <p style={{fontWeight:"bold",color:"orange"}}>Légendaire : 5% (1 Pokemon Légendaire)</p>
