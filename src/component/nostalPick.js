266cf9ed8e7818486d4122b0dd2e346f45ad1647
pierre schuvey <chromatykretro@gmail.com>
2023-05-08 21:15:20 +0200

    fix

--- src/component/nostalPick.js
+++ src/component/nostalPick.js
@@ -14,7 +14,7 @@ function NostalPick(props) {
         <h1 id="greenTitle">NOSTAL<br/>PICK</h1>
         <h1 id="pinkTitle">NOSTAL<br/>PICK</h1>
         <object style={{position:"absolute",top:"50px",zIndex:"-2",left: "1130px"}} data="télécharger.svg" width="150" height="300"> </object>
-      <div>
+      </div>
       <div id="one boxContainer" className="button2">
         <p className="nbBox">1</p>
         <object data="télécharger.svg" width="150" height="300"> </object>
