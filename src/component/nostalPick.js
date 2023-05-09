9099c8d2c2493bb67470b1fa289d11c1bfbea1aa
PierreSchuvey <chromatykretro@gmail.com>
2023-05-09 16:09:40 +0200

    fix

--- src/component/nostalPick.js
+++ src/component/nostalPick.js
@@ -5,8 +5,6 @@ import UniqueBox from "./UniqueBox";
 function NostalPick(props) {
   var consoles = ["GB","GBA","GBC","MASTER SYSTEM","MEGADRIVE","N64","NDS","NES","NGC","PS1","PSP","SNES"];
   var numbers = [1,2,3,4,5,6,7,8,9];
-  console.log(numbers);
-  console.log(consoles);
   return(
       <>
         <ul className="box-list">
