5524b1113baa1e634960cb72c43247ad707750b8
PierreSchuvey <chromatykretro@gmail.com>
2023-05-09 14:46:47 +0200

    fix

---
+++ src/component/UniqueBox.js
@@ -0,0 +1,10 @@
+import React,{useState, useEffect} from 'react';
+import Axios from 'axios'
+import '../App.css'
+
+function UniqueBox() {
+    reutrn(
+        <p>1</p>
+    )
+}
+export default UniqueBox
--- src/component/nostalPick.js
+++ src/component/nostalPick.js
@@ -1,26 +1,22 @@
 import React,{useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom'
 import ReactPaginate from 'react-paginate';
-import Axios from 'axios'
-import Pagination from './paginate.js';
-import CreateTrade from './createTrade.js';
-import '../App.css'
+import Axios from 'axios';
+import '../App.css';
+import UniqueBox from "./UniqueBox";
 import moment from 'moment';
 import $ from 'jquery';
 
-
-// function PickBox() {
-//   console.log(consoles);
-// }
 function NostalPick(props) {
   var consoles = ["GB","GBA","GBC","MASTER SYSTEM","MEGADRIVE","N64","NDS","NES","NGC","PS1","PSP","SNES"];
   function PickBox(){
       console.log(consoles);
   }
-  return(
-    <>
-        <div onclick={PickBox}>Test</div>
-    </>
-  )
+  for (var i = 0; i<= 8;i++){
+      return(
+          <>
+            <UniqueBox/>
+          </>
+      )
+  }
 }
 export default NostalPick
