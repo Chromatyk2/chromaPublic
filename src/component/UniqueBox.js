d836f33c1c32667668ca77ab96ba51783a4a509d
PierreSchuvey <chromatykretro@gmail.com>
2023-05-10 14:38:23 +0200

    fix

--- src/component/UniqueBox.js
+++ src/component/UniqueBox.js
@@ -65,7 +65,7 @@ function UniqueBox(props) {
             }, 1500);
         } else if (($("[checked=checked]").position().top > $("#centerBox").position().top) && ($("[checked=checked]").position().left > $("#centerBox").position().left)) {
             $("[checked=checked]").animate({"top": "-200px", "left": "-300px"}, 1500);
-            $("[checked=checked]").children(".button2").animate({"height": "500px", "width": "500px"}, 1500);
+            $("[checked=checked]").children(".button2").children(".button2").animate({"height": "500px", "width": "500px"}, 1500);
         } else if (($("[checked=checked]").position().top < $("#centerBox").position().top) && ($("[checked=checked]").position().left == $("#centerBox").position().left)) {
             $("[checked=checked]").animate({"top": "200px"}, 1500);
         } else if (($("[checked=checked]").position().top < $("#centerBox").position().top) && ($("[checked=checked]").position().left > $("#centerBox").position().left)) {
