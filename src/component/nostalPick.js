21f2c79f816e08f4ec269c646eb800fa067ff4fe
PierreSchuvey <chromatykretro@gmail.com>
2023-05-10 13:28:47 +0200

    fix

--- src/component/nostalPick.js
+++ src/component/nostalPick.js
@@ -18,14 +18,10 @@ function NostalPick(props) {
         $('audio#rainbowWin')[0].pause();
         $('audio#rainbowWin')[0].currentTime = 0;
         $('#containerGlobal').css("background-color","rgba(0,0,0,0)");
+        var others = $('.box-list li').not($("[checked=checked],[alreadyopen=alreadyopen]"));
         others.css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 1500);
-        $('[alreadyopen]').css("visibility","hidden")
-        setTimeout(function (){
-            $("[checked=checked]").css("visibility","hidden");
-            $("[checked=checked]").attr("alreadyopen","alreadyopen");
-            $("[checked=checked]").removeAttr("checked");
-        },1),
-            $(this).addClass('out');
+        $('[alreadyopen]').css("visibility","hidden");
+        $(this).addClass('out');
         $('body').removeClass('modal-active');
     };
   return(
