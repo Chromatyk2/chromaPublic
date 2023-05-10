45c054b6ad89dc835aa27f7b75a8e923f0bbcd96
PierreSchuvey <chromatykretro@gmail.com>
2023-05-10 09:36:55 +0200

    fix

--- src/component/UniqueBox.js
+++ src/component/UniqueBox.js
@@ -43,7 +43,7 @@ function UniqueBox(props) {
     var randomNumber = Math.floor(Math.random()*max) + 1;
     function displayNormalLaderboard(e) {
         var box = e.target;
-        box.parent(".button2").parent(".box-list li").attr('checked', 'checked');
+        box.attr('checked', 'checked');
         if (($("[checked=checked]").position().top < $("#centerBox").position().top) && ($("[checked=checked]").position().left < $("#centerBox").position().left)) {
             $("[checked=checked]").animate({
                 "top": $("#centerBox").position().top,
