fadca9f2eee1463b7b5f66c7615fe8c9348bc3fe
PierreSchuvey <chromatykretro@gmail.com>
2023-05-10 16:39:47 +0200

    lastGames

---
+++ src/component/lastGame.js
@@ -0,0 +1,25 @@
+import React,{useState, useEffect} from 'react';
+import '../App.css';
+import Axios from 'axios';
+import UniqueBox from "./UniqueBox";
+
+function LastGames(props) {
+    const [lastGames, setLastGames] = useState(null);
+    useEffect(() => {
+        Axios.get('/api/lastGame')
+            .then(function(response){
+                setLastGames(response.data[0].image);
+            })
+    }, [])
+    if(lastGames !== null){
+        {lastGames.map((val, key) => {
+            return(
+                <div className="lastGameContainer">
+                    <p>{val.title}</p>
+                    <p>{val.console}</p>
+                </div>
+            )
+        })}
+    }
+}
+export default LastGames
