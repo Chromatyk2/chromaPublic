3e2b5c6fe1103dd81ad1bfaa9f0f42d9a9689cb4
PierreSchuvey <chromatykretro@gmail.com>
2023-05-10 14:10:58 +0200

    fix

--- src/component/UniqueBox.js
+++ src/component/UniqueBox.js
@@ -60,15 +60,15 @@ function UniqueBox(props) {
         $("#nbBox"+boxNumber).parent(".button2").parent(".box-list li").attr('checked','checked');
         if (($("[checked=checked]").position().top < $("#centerBox").position().top) && ($("[checked=checked]").position().left < $("#centerBox").position().left)) {
             $("[checked=checked]").animate({
-                "top": "250px",
+                "top": "200px",
                 "left": "300px"
             }, 1500);
         } else if (($("[checked=checked]").position().top > $("#centerBox").position().top) && ($("[checked=checked]").position().left > $("#centerBox").position().left)) {
             $("[checked=checked]").animate({"top": "-200px", "left": "-300px"}, 1500);
         } else if (($("[checked=checked]").position().top < $("#centerBox").position().top) && ($("[checked=checked]").position().left == $("#centerBox").position().left)) {
-            $("[checked=checked]").animate({"top": "250px"}, 1500);
+            $("[checked=checked]").animate({"top": "200px"}, 1500);
         } else if (($("[checked=checked]").position().top < $("#centerBox").position().top) && ($("[checked=checked]").position().left > $("#centerBox").position().left)) {
-            $("[checked=checked]").animate({"top": "250px", "left": "-300px"}, 1500);
+            $("[checked=checked]").animate({"top": "200px", "left": "-300px"}, 1500);
         } else if (($("[checked=checked]").position().top == $("#centerBox").position().top) && ($("[checked=checked]").position().left < $("#centerBox").position().left)) {
             $("[checked=checked]").animate({"left": "300px"}, 1500);
         } else if (($("[checked=checked]").position().top == $("#centerBox").position().top) && ($("[checked=checked]").position().left > $("#centerBox").position().left)) {
