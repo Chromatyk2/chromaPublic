f29455b3efc8b35e85f0e54da78c33277429f244
PierreSchuvey <chromatykretro@gmail.com>
2023-05-10 13:26:18 +0200

    fix

--- src/component/nostalPick.js
+++ src/component/nostalPick.js
@@ -1,6 +1,7 @@
 import React,{useState, useEffect} from 'react';
 import '../App.css';
 import UniqueBox from "./UniqueBox";
+import $ from 'jquery';
 
 function NostalPick(props) {
   var consoles = ["GB","GBA","GBC","MASTER SYSTEM","MEGADRIVE","N64","NDS","NES","NGC","PS1","PSP","SNES"];
