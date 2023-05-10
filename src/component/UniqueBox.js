9e4474d35a7a5c2bbc5a355c7e9db098f90cc9ad
PierreSchuvey <chromatykretro@gmail.com>
2023-05-10 12:21:18 +0200

    fix

--- src/component/UniqueBox.js
+++ src/component/UniqueBox.js
@@ -73,8 +73,7 @@ function UniqueBox(props) {
             return;
         }
         setTimeout(function (){
-            let rare = Math.floor((Math.random() * 2) + 1);
-            console.log(rare);
+            let rare = Math.floor((Math.random() * 100) + 1);
             $("#nbBox"+boxNumber).animate(
                 { deg: 360 },
                 {
@@ -84,23 +83,25 @@ function UniqueBox(props) {
                     }
                 }
             );
-            // if(rare == rare){
-            //     let epic = Math.floor((Math.random() * 2) + 1);;
-            //     setTimeout(function (){
-            //         $("#nbBox"+boxNumber).toggleClass("rareBox");
-            //         $('#containerGlobal').toggleClass('shakeGreen');
-            //     },1000);
-            //     setTimeout(function (){
-            //         $("#nbBox"+boxNumber).animate(
-            //             { deg: 1440 },
-            //             {
-            //                 duration: 500,
-            //                 step: function(now) {
-            //                     $(this).css({ transform: 'rotate(' + now + 'deg)' });
-            //                 }
-            //             }
-            //         );
-            //     },2000);
+            if(rare < 99){
+                let epic = Math.floor((Math.random() * 2) + 1);
+                console.log(epic);
+                setTimeout(function (){
+                    $("#nbBox"+boxNumber).toggleClass("rareBox");
+                    $('#containerGlobal').toggleClass('shakeGreen');
+                },1000);
+                setTimeout(function (){
+                    $("#nbBox"+boxNumber).animate(
+                        { deg: 1440 },
+                        {
+                            duration: 500,
+                            step: function(now) {
+                                $(this).css({ transform: 'rotate(' + now + 'deg)' });
+                            }
+                        }
+                    );
+                },2000);
+            }
             //     if(epic == epic){
             //         let legendary = Math.floor((Math.random() * 5) + 1);;
             //         setTimeout(function (){
@@ -223,39 +224,6 @@ function UniqueBox(props) {
                     <div type="button" className={"button2 box closed"}  id={"nbBox"+props.number}></div>
                 </div>
             </li>
-            <div id="modal-container">
-                <div className="modal-background">
-                    <div style={{overflow:"inherit"}} className="modal">
-                        <p style={{fontSize:"65px",color:"white"}} className="looseTexte resultTexte">Perdu</p>
-                        <p style={{fontSize:"65px",color:"green"}} className="100Texte resultTexte">1 Pokemon Random !</p>
-                        <p style={{fontSize:"65px",color:"purple"}} className="500Texte resultTexte">1 Pokemon Taux shiny x 2 !!</p>
-                        <p style={{fontSize:"65px",color:"orange"}} className="1000Texte resultTexte">1 Pokemon Légendaire !!!</p>
-                        <p style={{fontSize:"65px",color:"rgba(0,0,0,0.3)"}} className="rainbow-text 5000Texte resultTexte">1 Pokemon shiny !!!!</p>
-                        <img id="imgModal" />
-                        <svg className="modal-svg" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none">
-                            <rect x="0" y="0" fill="none" width="226" height="162" rx="3" ry="3"></rect>
-                        </svg>
-                    </div>
-                </div>
-            </div>
-            <audio id="karateka">
-                <source src="sounds/karateka.mp3" type="audio/mpeg"/>
-            </audio>
-            <audio id="loose">
-                <source src="sounds/Loose.mp3" type="audio/mpeg"/>
-            </audio>
-            <audio id="greenWin">
-                <source src="sounds/GreenWin.mp3" type="audio/mpeg"/>
-            </audio>
-            <audio id="blueWin">
-                <source src="sounds/BlueWin.mp3" type="audio/mpeg"/>
-            </audio>
-            <audio id="orangeWin">
-                <source src="sounds/OrangeWin.mp3" type="audio/mpeg"/>
-            </audio>
-            <audio id="rainbowWin">
-                <source src="sounds/RainbowWin.mp3" type="audio/mpeg"/>
-            </audio>
         </>
     )
 }
