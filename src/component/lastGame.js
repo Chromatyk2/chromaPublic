09d7be20569eae383825f8eca66e7b4cc866bb73
PierreSchuvey <chromatykretro@gmail.com>
2023-05-11 14:38:58 +0200

    lastGames

--- src/component/lastGame.js
+++ src/component/lastGame.js
@@ -20,57 +20,59 @@ function LastGames(props) {
     //
     //     return () => clearInterval(interval);
     // }, []);
-    const addBtn = document.querySelector('.add-btn');
-    const list = document.querySelector('.list');
+    if(lastGames !== null){
+        const addBtn = document.querySelector('.add-btn');
+        const list = document.querySelector('.list');
 
-    const listItems = document.querySelectorAll('.list-item');
-    function calculateHeightOfListContainer(){
-        const firstListItem = listItems[0];
-        let heightOfListItem = firstListItem.clientHeight;
-        const styleTag = document.createElement('style');
-        document.body.prepend(styleTag);
-        styleTag.innerHTML = `.list-container.show {
+        const listItems = document.querySelectorAll('.list-item');
+        function calculateHeightOfListContainer(){
+            const firstListItem = listItems[0];
+            let heightOfListItem = firstListItem.clientHeight;
+            const styleTag = document.createElement('style');
+            document.body.prepend(styleTag);
+            styleTag.innerHTML = `.list-container.show {
         height: ${heightOfListItem}px;
     }`;
-        setTimeout(function(){
-            styleTag.innerHTML += `.list-container {
+            setTimeout(function(){
+                styleTag.innerHTML += `.list-container {
             transition: all 0.6s ease-out;
         }`;
-        }, 0);
-    };
-    calculateHeightOfListContainer();
+            }, 0);
+        };
+        calculateHeightOfListContainer();
 
-    function removeListItem(e){
-        let container = e.target;
-        while(!container.classList.contains('list-container')){
-            container = container.parentElement;
-        }
-        container.classList.remove('show');
-        const listItem = container.querySelector('.list-item');
-        listItem.classList.remove('show');
-        setTimeout(function(){
-            container.classList.remove('show');
-            container.ontransitionend = function(){
-                container.remove();
+        function removeListItem(e){
+            let container = e.target;
+            while(!container.classList.contains('list-container')){
+                container = container.parentElement;
             }
-        }, 350);
-    }
-    document.querySelectorAll('.list .list-container').forEach(function(container) {
-        container.onclick = removeListItem;
-    });
-
-    addBtn.onclick = function(e){
-        const container = document.createElement('li'); container.classList.add('list-container'); container.setAttribute('role', 'listitem');
-        const listItem = document.createElement('div'); listItem.classList.add('list-item'); listItem.innerHTML = 'List Item';
-        container.append(listItem);
-        addBtn.parentNode.insertBefore(container, addBtn);
-        container.onclick = removeListItem;
-        setTimeout(function(){
-            container.classList.add('show');
+            container.classList.remove('show');
+            const listItem = container.querySelector('.list-item');
+            listItem.classList.remove('show');
             setTimeout(function(){
-                listItem.classList.add('show');
+                container.classList.remove('show');
+                container.ontransitionend = function(){
+                    container.remove();
+                }
             }, 350);
-        }, 15);
+        }
+        document.querySelectorAll('.list .list-container').forEach(function(container) {
+            container.onclick = removeListItem;
+        });
+
+        addBtn.onclick = function(e){
+            const container = document.createElement('li'); container.classList.add('list-container'); container.setAttribute('role', 'listitem');
+            const listItem = document.createElement('div'); listItem.classList.add('list-item'); listItem.innerHTML = 'List Item';
+            container.append(listItem);
+            addBtn.parentNode.insertBefore(container, addBtn);
+            container.onclick = removeListItem;
+            setTimeout(function(){
+                container.classList.add('show');
+                setTimeout(function(){
+                    listItem.classList.add('show');
+                }, 350);
+            }, 15);
+        }
     }
     return(
         <>
