55399a46d878b5f6d2ee79a41d8b10b76ebc2a30
PierreSchuvey <chromatykretro@gmail.com>
2023-05-11 15:12:32 +0200

    lastGames

--- src/component/lastGame.js
+++ src/component/lastGame.js
@@ -11,81 +11,18 @@ function LastGames(props) {
                     setLastGames(response.data);
                 })
     }, []);
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         Axios.get('/api/lastGame')
-    //         .then(function(response){
-    //             setLastGames(response.data);
-    //         })
-    //     }, 10000);
-    //
-    //     return () => clearInterval(interval);
-    // }, []);
-    if(lastGames !== null){
-        function calculateHeightOfListContainer(){
-            const firstListItem = lastGames[0];
-            let heightOfListItem = firstListItem.clientHeight;
-            const styleTag = document.createElement('style');
-            document.body.prepend(styleTag);
-            styleTag.innerHTML = `.list-container.show {
-        height: ${heightOfListItem}px;
-    }`;
-            setTimeout(function(){
-                styleTag.innerHTML += `.list-container {
-            transition: all 0.6s ease-out;
-        }`;
-            }, 0);
-        };
-        calculateHeightOfListContainer();
+    useEffect(() => {
+        const interval = setInterval(() => {
+            Axios.get('/api/lastGame')
+            .then(function(response){
+                <p>{response.data[0].title}</p>
+            })
+        }, 10000);
 
-        function removeListItem(e){
-            let container = e.target;
-            while(!container.classList.contains('list-container')){
-                container = container.parentElement;
-            }
-            container.classList.remove('show');
-            const listItem = container.querySelector('.list-item');
-            listItem.classList.remove('show');
-            setTimeout(function(){
-                container.classList.remove('show');
-                container.ontransitionend = function(){
-                    container.remove();
-                }
-            }, 350);
-        }
-        document.querySelectorAll('.list .list-container').forEach(function(container) {
-            container.onclick = removeListItem;
-        });
-    }
-    function deploy(e){
-        console.log('yes');
-        const container = document.createElement('li'); container.classList.add('list-container'); container.setAttribute('role', 'listitem');
-        const listItem = document.createElement('div'); listItem.classList.add('list-item'); listItem.innerHTML = 'List Item';
-        container.append(listItem);
-        setTimeout(function(){
-            container.classList.add('show');
-            setTimeout(function(){
-                listItem.classList.add('show');
-            }, 350);
-        }, 15);
-    }
+        return () => clearInterval(interval);
+    }, []);
     return(
         <>
-            <ul className="list" aria-live="assertive">
-                <li className="list-container show">
-                    <div className="list-item show">List Item</div>
-                </li>
-                <li className="list-container show">
-                    <div className="list-item show">List Item</div>
-                </li>
-                <li className="list-container show">
-                    <div className="list-item show">List Item</div>
-                </li>
-                <li className="list-container show">
-                    <div className="list-item show">List Item</div>
-                </li>
-            </ul>
-            <button onClick={deploy} className="add-btn">Add New Item</button>
             {lastGames &&
                 lastGames.map((val, key) => {
                     return(
