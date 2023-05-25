a4c8456170f9cb9bc6b96d5e2949c8e341eed0c1
pierre schuvey <chromatykretro@gmail.com>
2023-05-25 23:01:09 +0200

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -6,9 +6,7 @@ import PkmList from './pkmList.js'
 function HomePage(props) {
   return (
     <>
-    {
-      props.cookies
-    }
+      <p>props.cookies</p>
       <div className="socialContainer">
         <p className="myNetworks">Mes reseaux</p>
         <a className="socialLink" target='_blank' href="https://discord.gg/8V6fyQdSCG"><i class="fa-brands fa-discord"></i>Discord</a>
