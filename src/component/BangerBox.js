9af2ccbe529a473462421fe82e93bf185e9a4a96
pierre schuvey <chromatykretro@gmail.com>
2023-06-04 12:18:48 +0200

    dox

--- src/component/BangerBox.js
+++ src/component/BangerBox.js
@@ -232,9 +232,10 @@ function BangerBox(props) {
 
     return(
         <>
-            <li onClick={displayNormalLaderboard} className={"uniqueBox"} id={"bangerBox"}>
+            <li onClick={displayNormalLaderboard} className={"uniqueBox"} id={props.number === 5 ? 'centerBox' : 'otherBox'}>
                 <div id="one" className="button2">
-                    <div type="button" className={"button2 box closed"}  id={"uniqueBangerBox"}></div>
+                    <p className="nbBox">{props.number}</p>
+                    <div type="button" className={"button2 box closed"}  id={"nbBox"+props.number}></div>
                 </div>
             </li>
         </>
