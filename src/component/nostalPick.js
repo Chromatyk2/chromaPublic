b63cbe9b8d2c648458bbdd2d1892b584d64119a4
pierre schuvey <chromatykretro@gmail.com>
2023-08-12 19:26:23 +0200

    fix

--- src/component/nostalPick.js
+++ src/component/nostalPick.js
@@ -31,6 +31,7 @@ function NostalPick(props) {
     };
   return(
       <>
+      <div id="containerGlobal"></div>
         <ul className="box-list">
           {numbers.map((val, key) => {
             var uniqueConsole = consoles[Math.floor(Math.random()*consoles.length)];
