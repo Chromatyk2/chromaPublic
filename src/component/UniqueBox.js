6a9db5d3905abc434c881965d9cc7e842e21de4b
pierre schuvey <chromatykretro@gmail.com>
2023-08-12 21:11:20 +0200

    fix

--- src/component/UniqueBox.js
+++ src/component/UniqueBox.js
@@ -227,7 +227,7 @@ function UniqueBox(props) {
                     $('audio#karateka')[0].currentTime = 0
                 },502);
             };
-        },1501);
+        },1000);
     }
 
     return(
