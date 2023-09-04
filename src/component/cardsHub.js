0f31c4d5a4aa418ce5231cf2f51f5c4a8219f4b2
pierre schuvey <chromatykretro@gmail.com>
2023-09-04 21:38:47 +0200

    Cards

--- src/component/cardsHub.js
+++ src/component/cardsHub.js
@@ -7,6 +7,23 @@ import '../App.css'
 import moment from 'moment';
 
 function CardsHub(props) {
+const [error, setError] = useState(null);
+const [isLoaded, setIsLoaded] = useState(false);
+const [items, setItems] = useState([]);
+  useEffect(() => {
+   fetch("https://pokeapi.co/api/v2/pokemon/"+randomNumber)
+     .then(res => res.json())
+     .then(
+       (result) => {
+         setIsLoaded(true);
+         setItems(result);
+       },
+       (error) => {
+         setIsLoaded(true);
+         setError(error);
+       }
+     )
+ }, [])
   return 'Yes'
 }
 export default CardsHub
