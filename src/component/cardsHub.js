5be0eeddbebca99a5a4428dd3fb7b487d25a8b0f
pierre schuvey <chromatykretro@gmail.com>
2023-09-11 14:09:35 +0200

    first commit

--- src/component/cardsHub.js
+++ src/component/cardsHub.js
@@ -2,7 +2,6 @@ import React,{useState, useEffect} from 'react';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import { useCookies } from 'react-cookie';
 import $ from "jquery";
-import './App.css';
 import 'react-tooltip/dist/react-tooltip.css'
 import MyCards from './myCards.js';
 import NavBar from "./navbar";
