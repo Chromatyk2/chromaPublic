78a334ae8df20e0d1308a7d23f9682f350410fcf
pierre schuvey <chromatykretro@gmail.com>
2023-09-12 16:57:37 +0200

    first commit

--- src/component/myBoosters.js
+++ src/component/myBoosters.js
@@ -25,12 +25,7 @@ function MyBoosters(props) {
     function openModal() {
         setIsOpen(true);
     }
-
-    function afterOpenModal() {
-        // references are now sync'd and can be accessed.
-        subtitle.style.color = '#f00';
-    }
-
+    
     function closeModal() {
         setIsOpen(false);
     }
