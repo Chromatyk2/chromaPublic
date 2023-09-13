e1419aad82883047bbac314b89bcb1db7d059412
pierre schuvey <chromatykretro@gmail.com>
2023-09-13 12:24:11 +0200

    first commit

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -12,7 +12,7 @@ function OpeningCards(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [error, setError] = useState(null);
     const [nbCards, setNbCards] = useState(0);
-    useEffect(() => {
+    if(nbCards < 4){
         fetch("https://api.tcgdex.net/v2/fr/cards/"+props.items[[Math.floor(Math.random() * props.items.length)]].id)
             .then(res => res.json())
             .then(
@@ -28,7 +28,7 @@ function OpeningCards(props) {
                     setError(error);
                 }
             )
-    }, [nbCards < 4]);
+    }
     console.log(tenCards)
     return (
         <>
