a20cb4d913d13229ba93900d239d32ef57653b6c
pierre schuvey <chromatykretro@gmail.com>
2023-09-18 21:17:04 +0200

    fix

--- src/component/myCardSet.js
+++ src/component/myCardSet.js
@@ -46,10 +46,10 @@ function MyCardsSet(props) {
                       if(myCardsId.includes(val.id)){
                         let cardNb = myCards.find((myCard) => myCard.card.includes(val.id));
                         return(
-                          <>
+                          <div className={"cardBox"}>
                             <p>{cardNb.nbCard}</p>
                             <img class="fit-picture" src={val.image+"/high.webp"} />
-                          </>
+                          </div>
                         )
                       }else{
                         return(
