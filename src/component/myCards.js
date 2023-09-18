6f0eeea40fc2d2a202dbc7d570591c92c63b620e
pierre schuvey <chromatykretro@gmail.com>
2023-09-18 16:54:20 +0200

    fix

--- src/component/myCards.js
+++ src/component/myCards.js
@@ -25,19 +25,19 @@ function MyCards(props) {
     return (
         <>
             <div id={"cardsContainer"}>
-                {nbCards &&
-                    page ?
+                { page ?
                         <MyCardsSet idBooster={page}/>
                     :
-                        nbCards.map((val, key) => {
-                            return(
-                                <div className="uniqueTradeContainer">
-                                    <img className="fit-picture" src={"https://images.pokemontcg.io/" + val.booster + "/logo.png"} alt="Grapefruit slice atop a pile of other slices"/>
-                                    <p className="pokemonNameTrade">{val.nbCard}</p>
-                                    <button value={val.booster} onClick={displayPage} className="guessTradeButton">Voir toute mes cartes</button>
-                                </div>
-                            )
-                        })
+                        nbCards &&
+                            nbCards.map((val, key) => {
+                                return(
+                                    <div className="uniqueTradeContainer">
+                                        <img className="fit-picture" src={"https://images.pokemontcg.io/" + val.booster + "/logo.png"} alt="Grapefruit slice atop a pile of other slices"/>
+                                        <p className="pokemonNameTrade">{val.nbCard}</p>
+                                        <button value={val.booster} onClick={displayPage} className="guessTradeButton">Voir toute mes cartes</button>
+                                    </div>
+                                )
+                            })
                 }
             </div>
         </>
