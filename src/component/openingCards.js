63b3b1806e415fffa25684b173e6e12c9f5fd377
pierre schuvey <chromatykretro@gmail.com>
2023-09-18 15:07:10 +0200

    fix

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -95,7 +95,7 @@ function OpeningCards(props) {
             {tenCards &&
                 tenCards.slice(0).reverse().map((val, key) => {
                     return(
-                        <img value={val.id} onClick={((e) => this.getCard(e, data))} onClick={() => getCard(val.id)} class={isHidden === true ? "fit-picture dropCards hiddenCards" : endPull === true ? "fit-picture dropCards showCards gettedCard endPull" : "fit-picture dropCards showCards"} src={val.image+"/high.webp"} alt="Grapefruit slice atop a pile of other slices"/>
+                        <img value={val.id} onClick={((e) => this.getCard(e, val.id))} onClick={() => getCard(val.id)} class={isHidden === true ? "fit-picture dropCards hiddenCards" : endPull === true ? "fit-picture dropCards showCards gettedCard endPull" : "fit-picture dropCards showCards"} src={val.image+"/high.webp"} alt="Grapefruit slice atop a pile of other slices"/>
                     )
                 })
             }
