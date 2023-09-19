b94aa13bc523e9f356b88285e8903306b2fae51a
pierre schuvey <chromatykretro@gmail.com>
2023-09-19 10:15:41 +0200

    fix

--- src/component/myCards.js
+++ src/component/myCards.js
@@ -7,13 +7,11 @@ import '../App.css'
 import moment from 'moment';
 import MyCardsSet from './myCardSet.js';
 
-
 function MyCards(props) {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [nbCards, setNbCards] = useState(null);
     const [page, setPage] = useState(null);
-    const [nbCard, setNbCard] = useState(null);
     function displayPage(e) {
         setPage(e.target.value)
         setNbCard(e.target.getAttribute("nbCard"))
@@ -30,26 +28,26 @@ function MyCards(props) {
     }, [])
     return (
         <>
-            {nbCards &&
-                page ?
+                { page ?
                       <>
                         <button onClick={backPage} className="guessTradeButton">Retour</button>
                         <MyCardsSet user={props.user} idBooster={page}/>
                       </>
                 :
                     <div id={"cardsContainer"}>
-
-                        {nbCards.map((val, key) => {
+                        {nbCards &&
+                            nbCards.map((val, key) => {
                                 return(
                                     <div className="uniqueTradeContainer">
                                         <img className="fit-picture" src={"https://images.pokemontcg.io/" + val.booster + "/logo.png"} alt="Grapefruit slice atop a pile of other slices"/>
                                         <p className="pokemonNameTrade">{val.nbCard} carte(s)</p>
-                                        <button nbCard={val.nbCard} value={val.booster} onClick={displayPage} className="guessTradeButton">Voir toute mes cartes</button>
+                                        <button value={val.booster} onClick={displayPage} className="guessTradeButton">Voir toute mes cartes</button>
                                     </div>
                                 )
                             })}
+
                     </div>
-            }
+                }
         </>
     )
 }
