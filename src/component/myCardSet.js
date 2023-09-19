a3d56e5a161d5b8751630ab50556149eb1f4bde2
pierre schuvey <chromatykretro@gmail.com>
2023-09-19 13:31:24 +0200

    fix

--- src/component/myCardSet.js
+++ src/component/myCardSet.js
@@ -40,7 +40,7 @@ function MyCardsSet(props) {
         setMyCardsId(myCardsId => [...myCardsId,val.card]);
       })
     }, [myCards]);
-
+console.log(totalNb);
     return (
         <>
             {totalNb &&
