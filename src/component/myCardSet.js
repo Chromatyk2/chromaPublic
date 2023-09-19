857f94b39db8ec176110f8825f3c93329c2a775a
pierre schuvey <chromatykretro@gmail.com>
2023-09-19 11:41:09 +0200

    fix

--- src/component/myCardSet.js
+++ src/component/myCardSet.js
@@ -39,6 +39,12 @@ function MyCardsSet(props) {
       })
     }, [myCards]);
     return (
+        <>
+            {items &&
+                <div className="fullProgressBar">
+                    <div style="width:25%">{items.cardCount.total+"/"+props.card}</div>
+                 </div>
+            }
             <div id={"cardsContainer"}>
                 {items &&
                     items.cards.map((val, key) => {
@@ -58,6 +64,7 @@ function MyCardsSet(props) {
                     })
                 }
             </div>
+        </>
     )
 }
 export default MyCardsSet
