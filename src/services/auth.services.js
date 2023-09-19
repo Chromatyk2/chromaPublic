0454cbda6ee4be93e68b84d41b29ea8ed970b142
pierre schuvey <chromatykretro@gmail.com>
2023-09-19 16:45:03 +0200

    fix

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -53,8 +53,7 @@ function AuthService() {
           client_id:"7kpmnnx1qptajh5ow3lylsws13u2op",
           client_secret:"1byi9sa1ch1g3mmgd9aibfzzr5ovbp",
           code:params.code,
-          grant_type:"authorization_code",
-          redirect_uri:"https://chromatyk.fr/"
+          grant_type:"authorization_code"
         }
       )
       .then(
