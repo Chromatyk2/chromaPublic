77a54cd3e2773e8de787e25b0e2bac208cc9ffca
pierre schuvey <chromatykretro@gmail.com>
2023-09-19 15:12:04 +0200

    fix

--- src/component/progressBarCard.js
+++ src/component/progressBarCard.js
@@ -16,7 +16,7 @@ function ProgressBarCard(props) {
         borderRadius: '50px'
     },
     intBar: {
-        width: parseFloat(props.getNb/props.item.items.cardCount.total*100).toFixed(2)+"px",
+        width: "50px",
         position: 'relative',
         background: '#0a3a2c',
         textWrap: 'nowrap',
