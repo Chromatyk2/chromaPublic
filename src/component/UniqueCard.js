b32788113746cc3fdede72d7a8100cd32f0bc341
pierre schuvey <chromatykretro@gmail.com>
2023-09-25 14:05:18 +0200

    fix

--- src/component/UniqueCard.js
+++ src/component/UniqueCard.js
@@ -23,10 +23,18 @@ function UniqueCard(props) {
     };
         return (
             <>
-                <div class={"myCardsContainer"}>
-                    <img style={customStyles.image} className="fit-picture-card" src={props.cardImage + "/high.webp"}/>
-                    <p style={customStyles.content}>X {props.cardNb}</p>
+                <div className="card">
+                    <div className="wrapper">
+                        <img src={props.cardImage + "/high.webp"}
+                             className="cover-image"/>
+                    </div>
+                    <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp"
+                         className="character"/>
                 </div>
+                {/*<div class={"myCardsContainer"}>*/}
+                {/*    <img style={customStyles.image} className="fit-picture-card" src={props.cardImage + "/high.webp"}/>*/}
+                {/*    <p style={customStyles.content}>X {props.cardNb}</p>*/}
+                {/*</div>*/}
             </>
         )
 }
