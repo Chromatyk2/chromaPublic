c39f4ec45802e203f0ec058c4ca9c9c033f0f4fc
pierre schuvey <chromatykretro@gmail.com>
2023-09-27 14:00:30 +0200

    fix

--- src/component/cardsHub.js
+++ src/component/cardsHub.js
@@ -28,7 +28,7 @@ function CardsHub(props) {
             })
     }, [])
     useEffect(() => {
-        if(timer !== null){
+        if(timer !== null && time.length > 0){
             var myTimestamp = (new Date(timer[0].hour).getTime() / 1000) - 7200;
             var twoHour = ((new Date().getTime() / 1000));
             console.log(twoHour);
