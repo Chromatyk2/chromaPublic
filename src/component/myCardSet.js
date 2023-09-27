3ef4644d49a6f5a97fa7ac2a4efba6021426f768
pierre schuvey <chromatykretro@gmail.com>
2023-09-27 22:43:12 +0200

    fix

--- src/component/myCardSet.js
+++ src/component/myCardSet.js
@@ -95,7 +95,7 @@ function MyCardsSet(props) {
                         let cardNb = myCards.find((myCard) => myCard.card.includes(val.id));
                         return(
                           <button style={customStyles.buttonMyCard} onClick={openModal} className={"cardBox"}>
-                            <img cardId={val.id} pokemonId={val.dexId} myCardNb={cardNb.nbCard} image={val.image} class="fit-picture-card" src={"https://images.pokemoncard.io/images/"+props.idBooster+"/"+val.id+"_hiresopt.jpg" } onError={"this.onerror=null; this.src='https://images.pokemoncard.io/images/"+props.idBooster+"/"+val.id+".png'"} />
+                            <img cardId={val.id} pokemonId={val.dexId} myCardNb={cardNb.nbCard} image={val.image} class="fit-picture-card" src={"https://images.pokemoncard.io/images/"+props.idBooster+"/"+val.id+"_hiresopt.jpg" } onError={(e) => {e.target.onerror = null, e.target.src = "https://images.pokemoncard.io/images/"+props.idBooster+"/"+val.id+".png"}} />
                           </button>
                         )
                       }else{
