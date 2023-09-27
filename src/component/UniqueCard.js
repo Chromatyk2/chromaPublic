3b7454801caa4f2c163360d088abd6532387b962
pierre schuvey <chromatykretro@gmail.com>
2023-09-28 00:18:33 +0200

    fix

--- src/component/UniqueCard.js
+++ src/component/UniqueCard.js
@@ -29,13 +29,12 @@ function UniqueCard(props) {
                 (result) => {
                     setIsLoaded(true);
                     setPkm(result);
-                    console.log(pkm);
-                    console.log(result);
-                    if(result.rarity == "Ultra Rare"  || result.rarity == "Trainer Gallery Rare Holo"  || result.rarity == "Special Illustration"  || result.rarity == "Special Illustration Rare"  || result.rarity == "Rare BREAK" || result.rarity == "Illustration Rare" || result.rarity == "Hyper Rare"  || result.rarity == "LEGEND" || result.rarity == "Promo" || result.rarity == "Rare Holo GX" || result.rarity == "Rare Holo V" || result.rarity == "Rare Holo VMAX" || result.rarity == "Rare Rainbow" || result.rarity == "Rare Secret" || result.rarity == "Rare Shiny GX" || result.rarity == "Rare Ultra"){
+                    console.log(result.data);
+                    if(result.datararity == "Ultra Rare"  || result.datararity == "Trainer Gallery Rare Holo"  || result.datararity == "Special Illustration"  || result.datararity == "Special Illustration Rare"  || result.datararity == "Rare BREAK" || result.datararity == "Illustration Rare" || result.datararity == "Hyper Rare"  || result.datararity == "LEGEND" || result.datararity == "Promo" || result.datararity == "Rare Holo GX" || result.datararity == "Rare Holo V" || result.datararity == "Rare Holo VMAX" || result.datararity == "Rare Rainbow" || result.datararity == "Rare Secret" || result.datararity == "Rare Shiny GX" || result.datararity == "Rare Ultra"){
                         setGlow("bigImageRainbow")
-                    }else if(result.rarity == "Classic Collection"  || result.rarity == "Promo"  || result.rarity == "Radiant Rare"  || result.rarity == "Double Rare"  || result.rarity == "Amazing Rare" || result.rarity == "Promo" || result.rarity == "Rare ACE" || result.rarity == "Rare Holo" || result.rarity == "Rare Holo Star" || result.rarity == "Rare Holo LV.X" || result.rarity == "Rare Holo" || result.rarity == "Rare Holo EX" || result.rarity == "Rare Prime" || result.rarity == "Rare Prism Star" || result.rarity == "Rare Shining" || result.rarity == "Rare Shiny"){
+                    }else if(result.datararity == "Classic Collection"  || result.datararity == "Promo"  || result.datararity == "Radiant Rare"  || result.datararity == "Double Rare"  || result.datararity == "Amazing Rare" || result.datararity == "Promo" || result.datararity == "Rare ACE" || result.datararity == "Rare Holo" || result.datararity == "Rare Holo Star" || result.datararity == "Rare Holo LV.X" || result.datararity == "Rare Holo" || result.datararity == "Rare Holo EX" || result.datararity == "Rare Prime" || result.datararity == "Rare Prism Star" || result.datararity == "Rare Shining" || result.datararity == "Rare Shiny"){
                         setGlow("bigImageGold")
-                    }else if(result.rarity == "Rare"){
+                    }else if(result.datararity == "Rare"){
                         setGlow("bigImageRare")
                     }else{
                         setGlow("")
