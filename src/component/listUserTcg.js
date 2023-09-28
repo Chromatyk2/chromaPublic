375b48de7f69850948b5e3604c7b1e078c79629a
pierre schuvey <chromatykretro@gmail.com>
2023-09-28 13:23:36 +0200

    fix

---
+++ src/component/listUserTcg.js
@@ -0,0 +1,42 @@
+import React,{useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom'
+import ReactPaginate from 'react-paginate';
+import Axios from 'axios'
+import Pagination from './paginate.js';
+import '../App.css'
+import moment from 'moment';
+import Modal from 'react-modal';
+import OpeningBooster from "./openingBooster";
+
+function ListUserTcg(props) {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [list, setList] = useState(null);
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    const [boosterId, setBoosterId] = React.useState(null);
+
+    useEffect(() => {
+        Axios
+            .get("/api/getListUser")
+            .then(function(response){
+                setList(response.data);
+            })
+    }, [])
+
+    return (
+        <>
+            <div id={"cardsContainer"}>
+                {list &&
+                    list.map((val, key) => {
+                        return(
+                            <div className="uniqueTradeContainer">
+                                <p>{val.user}+" "+{val.nbCardUser}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
+export default ListUserTcg
