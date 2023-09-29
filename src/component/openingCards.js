bfca36699c778cd1893b812faf2eac658bc979b0
pierre schuvey <chromatykretro@gmail.com>
2023-09-29 21:59:29 +0200

    p

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -23,6 +23,7 @@ function OpeningCards(props) {
                     (result) => {
                         console.log(props.items.filter(item => item.rarity == 'Common'));
                         console.log(props.items.filter(item => item.rarity == 'Uncommon'));
+                        console.log(props.items.filter(item => item.rarity == 'Rare'));
                         if(tenCards.length < 7){
                             if(result.data.rarity == "Common"){
                                 setIsLoaded(true);
