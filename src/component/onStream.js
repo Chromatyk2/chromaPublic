311332ab0247697939672e379d6bb8bacc1ba11f
pierre schuvey <chromatykretro@gmail.com>
2023-10-01 19:50:46 +0200

    on air

--- src/component/onStream.js
+++ src/component/onStream.js
@@ -32,7 +32,7 @@ function OnStream(props) {
                 stream.data.length > 0 ?
                     <a className={"linkOnAir"} href={"https://twitch.tv/chromatyk"} target={"_blank"}>Live On <span className={"spanOnair"}>(clique et viens gagner des points)</span></a>
                 :
-                    <a className={"linkOnAirOff"} href={"https://twitch.tv/chromatyk"} target={"_blank"}>Live Off <span className={"spanOnair"}>(clique et lache ton follow ça fais plaisir)</span></a>
+                    <a className={"linkOnAirOff"} href={"https://twitch.tv/chromatyk"} target={"_blank"}>Live Off <span className={"spanOnair"}>(clique et lache ton follow ça fait plaisir)</span></a>
             }
         </>
 
