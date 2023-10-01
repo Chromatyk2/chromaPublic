6ac3ed0b66ce2c0038888da461fa836c5c54ec64
pierre schuvey <chromatykretro@gmail.com>
2023-10-01 14:18:44 +0200

    fix

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -17,79 +17,72 @@ function OpeningCards(props) {
     const [endPull, setEndPull] = React.useState(false)
     useEffect(() => {
         if (tenCards.length < 11) {
-            fetch("https://api.pokemontcg.io/v2/cards/"+props.items[[Math.floor(Math.random() * props.items.length)]].id)
-                .then(res => res.json())
-                .then(
-                    (result) => {
-                        console.log(props.items.filter(item => item.rarity == 'Common'));
-                        console.log(props.items.filter(item => item.rarity == 'Uncommon'));
-                        console.log(props.items.filter(item => item.rarity == 'Rare'));
-                        if(tenCards.length < 7){
-                            const commonArray = props.items.filter(item => item.rarity == 'Common');
-                            const randomCommon = commonArray[Math.floor(Math.random() * commonArray.length)];
-                                setIsLoaded(true);
-                                setTenCards(tenCards => [...tenCards,randomCommon]);
-                                setNbCards (nbCards + 1);
-                        }else if(tenCards.length > 6 && tenCards.length < 8){
-                            const uncommonArray = props.items.filter(item => item.rarity == 'Uncommon');
-                            const randomUncommon = uncommonArray[Math.floor(Math.random() * uncommonArray.length)];
-                                setIsLoaded(true);
-                                setTenCards(tenCards => [...tenCards,randomUncommon]);
-                                setNbCards (nbCards + 1);
-                        }else if(tenCards.length == 8){
-                            var bonus = Math.floor(Math.random() * 10);
-                            if(bonus == 0){
-                                const epicArray = props.items.filter(item => item.rarity =="Rare"  || item.rarity == "Classic Collection"  || item.rarity == "Promo"  || item.rarity == "Radiant Rare"  || item.rarity == "Double Rare"  || item.rarity == "Amazing Rare" || item.rarity == "Promo" || item.rarity == "Rare ACE" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo Star" || item.rarity == "Rare Holo LV.X" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo EX" || item.rarity == "Rare Prime" || item.rarity == "Rare Prism Star" || item.rarity == "Rare Shining" || item.rarity == "Rare Shiny");
-                                const randomEpic = epicArray[Math.floor(Math.random() * epicArray.length)];
-                                setTenCards(tenCards => [...tenCards,randomEpic]);
-                                setNbCards (nbCards + 1);
-                                setIsLoaded(false);
-                            }else{
-                                const uncommonArray = props.items.filter(item => item.rarity == 'Uncommon');
-                                const randomUncommon = uncommonArray[Math.floor(Math.random() * uncommonArray.length)];
-                                setIsLoaded(true);
-                                setTenCards(tenCards => [...tenCards,randomUncommon]);
-                                setNbCards (nbCards + 1);
-                            }
-                        }else if(tenCards.length == 9){
-                            var holo = Math.floor(Math.random() * 5);
-                            if(holo == 0){
-                                var ultra = Math.floor(Math.random() * 2);
-                                if(ultra == 0){
-                                        const ultraArray = props.items.filter(item => item.rarity =="Rare"  || item.rarity == "Classic Collection"  || item.rarity == "Promo"  || item.rarity == "Radiant Rare"  || item.rarity == "Double Rare"  || item.rarity == "Amazing Rare" || item.rarity == "Promo" || item.rarity == "Rare ACE" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo Star" || item.rarity == "Rare Holo LV.X" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo EX" || item.rarity == "Rare Prime" || item.rarity == "Rare Prism Star" || item.rarity == "Rare Shining" || item.rarity == "Rare Shiny"  || item.rarity == "Ultra Rare"  || item.rarity == "Trainer Gallery Rare Holo"  || item.rarity == "Special Illustration"  || item.rarity == "Special Illustration Rare"  || item.rarity == "Rare BREAK" || item.rarity == "Illustration Rare" || item.rarity == "Hyper Rare"  || item.rarity == "LEGEND" || item.rarity == "Promo" || item.rarity == "Rare Holo GX" || item.rarity == "Rare Holo V" || item.rarity == "Rare Holo VMAX" || item.rarity == "Rare Rainbow" || item.rarity == "Rare Secret" || item.rarity == "Rare Shiny GX" || item.rarity == "Rare Ultra");
-                                        const randomUltra = ultraArray[Math.floor(Math.random() * ultraArray.length)];
-                                        setTenCards(tenCards => [...tenCards,randomUltra]);
-                                        setNbCards (nbCards + 1);
-                                        setIsLoaded(false);
-                                }else{
-                                        const epicArray = props.items.filter(item => item.rarity =="Rare"  || item.rarity == "Classic Collection"  || item.rarity == "Promo"  || item.rarity == "Radiant Rare"  || item.rarity == "Double Rare"  || item.rarity == "Amazing Rare" || item.rarity == "Promo" || item.rarity == "Rare ACE" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo Star" || item.rarity == "Rare Holo LV.X" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo EX" || item.rarity == "Rare Prime" || item.rarity == "Rare Prism Star" || item.rarity == "Rare Shining" || item.rarity == "Rare Shiny");
-                                        const randomEpic = epicArray[Math.floor(Math.random() * epicArray.length)];
-                                        setTenCards(tenCards => [...tenCards,randomEpic]);
-                                        setNbCards (nbCards + 1);
-                                        setIsLoaded(false);
-                                }
-                            }else{
-                                    const rareArray = props.items.filter(item => item.rarity =="Rare");
-                                    const randomRare = rareArray[Math.floor(Math.random() * rareArray.length)];
-                                    setTenCards(tenCards => [...tenCards,randomRare]);
-                                    setNbCards (nbCards + 1);
-                                    setIsLoaded(false);
-                            }
-                        }
+            if(tenCards.length < 7){
+                const commonArray = props.items.filter(item => item.rarity == 'Common');
+                const randomCommon = commonArray[Math.floor(Math.random() * commonArray.length)];
+                setIsLoaded(true);
+                setTenCards(tenCards => [...tenCards,randomCommon]);
+                setNbCards (nbCards + 1);
+            }else if(tenCards.length > 6 && tenCards.length < 8){
+                const uncommonArray = props.items.filter(item => item.rarity == 'Uncommon');
+                const randomUncommon = uncommonArray[Math.floor(Math.random() * uncommonArray.length)];
+                setIsLoaded(true);
+                setTenCards(tenCards => [...tenCards,randomUncommon]);
+                setNbCards (nbCards + 1);
+            }else if(tenCards.length == 8){
+                var bonus = Math.floor(Math.random() * 10);
+                if(bonus == 0){
+                    const epicArray = props.items.filter(item => item.rarity =="Rare"  || item.rarity == "Classic Collection"  || item.rarity == "Promo"  || item.rarity == "Radiant Rare"  || item.rarity == "Double Rare"  || item.rarity == "Amazing Rare" || item.rarity == "Promo" || item.rarity == "Rare ACE" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo Star" || item.rarity == "Rare Holo LV.X" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo EX" || item.rarity == "Rare Prime" || item.rarity == "Rare Prism Star" || item.rarity == "Rare Shining" || item.rarity == "Rare Shiny");
+                    const randomEpic = epicArray[Math.floor(Math.random() * epicArray.length)];
+                    setTenCards(tenCards => [...tenCards,randomEpic]);
+                    setNbCards (nbCards + 1);
+                    setIsLoaded(false);
+                }else{
+                    const uncommonArray = props.items.filter(item => item.rarity == 'Uncommon');
+                    const randomUncommon = uncommonArray[Math.floor(Math.random() * uncommonArray.length)];
+                    setIsLoaded(true);
+                    setTenCards(tenCards => [...tenCards,randomUncommon]);
+                    setNbCards (nbCards + 1);
+                }
+            }else if(tenCards.length == 9){
+                var holo = Math.floor(Math.random() * 5);
+                if(holo == 0){
+                    var ultra = Math.floor(Math.random() * 2);
+                    if(ultra == 0){
+                        const ultraArray = props.items.filter(item => item.rarity =="Rare"  || item.rarity == "Classic Collection"  || item.rarity == "Promo"  || item.rarity == "Radiant Rare"  || item.rarity == "Double Rare"  || item.rarity == "Amazing Rare" || item.rarity == "Promo" || item.rarity == "Rare ACE" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo Star" || item.rarity == "Rare Holo LV.X" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo EX" || item.rarity == "Rare Prime" || item.rarity == "Rare Prism Star" || item.rarity == "Rare Shining" || item.rarity == "Rare Shiny"  || item.rarity == "Ultra Rare"  || item.rarity == "Trainer Gallery Rare Holo"  || item.rarity == "Special Illustration"  || item.rarity == "Special Illustration Rare"  || item.rarity == "Rare BREAK" || item.rarity == "Illustration Rare" || item.rarity == "Hyper Rare"  || item.rarity == "LEGEND" || item.rarity == "Promo" || item.rarity == "Rare Holo GX" || item.rarity == "Rare Holo V" || item.rarity == "Rare Holo VMAX" || item.rarity == "Rare Rainbow" || item.rarity == "Rare Secret" || item.rarity == "Rare Shiny GX" || item.rarity == "Rare Ultra");
+                        const randomUltra = ultraArray[Math.floor(Math.random() * ultraArray.length)];
+                        setTenCards(tenCards => [...tenCards,randomUltra]);
+                        setNbCards (nbCards + 1);
+                        setIsLoaded(false);
+                    }else{
+                        const epicArray = props.items.filter(item => item.rarity =="Rare"  || item.rarity == "Classic Collection"  || item.rarity == "Promo"  || item.rarity == "Radiant Rare"  || item.rarity == "Double Rare"  || item.rarity == "Amazing Rare" || item.rarity == "Promo" || item.rarity == "Rare ACE" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo Star" || item.rarity == "Rare Holo LV.X" || item.rarity == "Rare Holo" || item.rarity == "Rare Holo EX" || item.rarity == "Rare Prime" || item.rarity == "Rare Prism Star" || item.rarity == "Rare Shining" || item.rarity == "Rare Shiny");
+                        const randomEpic = epicArray[Math.floor(Math.random() * epicArray.length)];
+                        setTenCards(tenCards => [...tenCards,randomEpic]);
+                        setNbCards (nbCards + 1);
+                        setIsLoaded(false);
+                    }
+                }else{
+                    const rareArray = props.items.filter(item => item.rarity =="Rare");
+                    const randomRare = rareArray[Math.floor(Math.random() * rareArray.length)];
+                    setTenCards(tenCards => [...tenCards,randomRare]);
+                    setNbCards (nbCards + 1);
+                    setIsLoaded(false);
+                }
+            }
         }
     }, [nbCards])
     function showCards() {
         setIsHidden(false);
     }
     function getCard(e) {
-            Axios.post('/api/addCard',
-                {
-                    pseudo:props.user,
-                    idCard:e.target.getAttribute("cardId"),
-                    booster:props.idBooster
-                })
-            e.target.classList.toggle('gettedCard');
-            setIndex(index + 1);
+        Axios.post('/api/addCard',
+            {
+                pseudo:props.user,
+                idCard:e.target.getAttribute("cardId"),
+                booster:props.idBooster
+            })
+        e.target.classList.toggle('gettedCard');
+        setIndex(index + 1);
     }
 
     function getLastCard(e) {
@@ -121,11 +114,11 @@ function OpeningCards(props) {
     return (
         <>
             {tenCards.length == 10 &&
-            <div onClick={showCards} class={isHidden === true ? "dropBooster fit-picture showBooster" : "fit-picture dropCards hiddenBooster"}>
-                <img style={customStyles.imgModal} src={"https://images.pokemontcg.io/" + props.idBooster + "/logo.png"} alt="Grapefruit slice atop a pile of other slices"/>
-                <p style={customStyles.textModal}>Appuie pour découvrir tes cartes</p>
+                <div onClick={showCards} class={isHidden === true ? "dropBooster fit-picture showBooster" : "fit-picture dropCards hiddenBooster"}>
+                    <img style={customStyles.imgModal} src={"https://images.pokemontcg.io/" + props.idBooster + "/logo.png"} alt="Grapefruit slice atop a pile of other slices"/>
+                    <p style={customStyles.textModal}>Appuie pour découvrir tes cartes</p>
 
-            </div>
+                </div>
             }
             {tenCards.length == 10 &&
                 tenCards.slice(0).reverse().map((val, key) => {
