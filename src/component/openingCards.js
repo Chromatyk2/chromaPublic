3a11eebb4bd1b6ec13a2fd3755b62a6b823d5a99
pierre schuvey <chromatykretro@gmail.com>
2023-10-02 16:57:50 +0200

    on air

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -136,7 +136,7 @@ function OpeningCards(props) {
         var nextId = parseInt(id,10) - 1;
         var next = document.getElementById("cardNb"+nextId);
         var rarity = next.getAttribute("rarity");
-        var nextCardId = nex.getAttribute("cardId");
+        var nextCardId = next.getAttribute("cardId");
         if(rarity == "Rare"){
             next.classList.toggle('glowGetBlue');
         }else if(rarity =="Rare"  || rarity == "Classic Collection"  || rarity == "Promo"  || rarity == "Radiant Rare"  || rarity == "Double Rare"  || rarity == "Amazing Rare" || rarity == "Promo" || rarity == "Rare ACE" || rarity == "Rare Holo" || rarity == "Rare Holo Star" || rarity == "Rare Holo LV.X" || rarity == "Rare Holo" || rarity == "Rare Holo EX" || rarity == "Rare Prime" || rarity == "Rare Prism Star" || rarity == "Rare Shining" || rarity == "Rare Shiny" || rarity == "Rare Holo V"){
