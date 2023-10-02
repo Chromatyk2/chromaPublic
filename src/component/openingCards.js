b5e625b87d92b479986f68867dff780886d54da2
pierre schuvey <chromatykretro@gmail.com>
2023-10-02 15:58:35 +0200

    on air

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -153,13 +153,15 @@ function OpeningCards(props) {
             }
             {tenCards.length == 10 &&
                 tenCards.slice(0).reverse().map((val, key) => {
-                    return(
-                                <img style={{display: key < 9 && "none"}} id={"cardNb" + key} keyCard={key}
-                                     cardId={val.id} onClick={key == 0 ? getLastCard : getCard}
-                                     className={isHidden === true ? "fit-picture dropCards hiddenCards" : endPull === true ? "fit-picture dropCards showCards gettedCard endPull" : key == 9 ? "fit-picture dropCards showCards glowGet" : "fit-picture dropCards glowGet"}
-                                     src={"https://images.pokemoncard.io/images/" + props.idBooster + "/" + val.id + "_hiresopt.jpg"}
-                                     onError={errorImage} alt="Grapefruit slice atop a pile of other slices"/>
-                    )
+                        return(
+                            <>
+                            <img rarity={val.rarity} style={{display: key < 9 && "none"}} id={"cardNb" + key} keyCard={key}
+                                 cardId={val.id} onClick={key == 0 ? getLastCard : getCard}
+                                 className={isHidden === true ? "fit-picture dropCards hiddenCards" : endPull === true ? "fit-picture dropCards showCards gettedCard endPull" : key == 9 ? "fit-picture dropCards showCards glowGet" : "fit-picture dropCards glowGet"}
+                                 src={"https://images.pokemoncard.io/images/" + props.idBooster + "/" + val.id + "_hiresopt.jpg"}
+                                 onError={errorImage} alt="Grapefruit slice atop a pile of other slices"/>
+                            </>
+                        )
                 })
             }
             {tenCards.length < 10 &&
