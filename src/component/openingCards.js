a77e2e7172a442484616067a90c01ec0c4269b2b
pierre schuvey <chromatykretro@gmail.com>
2023-10-02 14:23:27 +0200

    on air

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -155,7 +155,7 @@ function OpeningCards(props) {
                     return(
                                 <img style={{display: key < 9 && "none"}} id={"cardNb" + key} keyCard={key}
                                      cardId={val.id} onClick={key == 0 ? getLastCard : getCard}
-                                     className={isHidden === true ? "fit-picture dropCards hiddenCards" : endPull === true ? "fit-picture dropCards showCards gettedCard endPull" : key == 9 ? "fit-picture dropCards showCards" : "fit-picture dropCards "}
+                                     className={isHidden === true ? "fit-picture dropCards hiddenCards" : endPull === true ? "fit-picture dropCards showCards gettedCard endPull" : key == 9 ? "fit-picture dropCards showCards glowGet" : "fit-picture dropCards glowGet"}
                                      src={"https://images.pokemoncard.io/images/" + props.idBooster + "/" + val.id + "_hiresopt.jpg"}
                                      onError={errorImage} alt="Grapefruit slice atop a pile of other slices"/>
                     )
