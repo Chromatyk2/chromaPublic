19874ce5ecc974a3bf0fac3514aa0d971967d93d
pierre schuvey <chromatykretro@gmail.com>
2023-10-04 12:27:46 +0200

    sell

--- src/component/myCardSell.js
+++ src/component/myCardSell.js
@@ -73,12 +73,14 @@ function MyCardSell(props) {
     function handleClick(e) {
         var cardId = e.target.getAttribute("cardId");
         var cardNb = e.target.getAttribute("myCardNb");
+        const cartItemIndex = cardToSell.findIndex(item => item.card === cardId);
         if(cardToSell.length > 0){
-            if(cardToSell.find((card) => card.card.includes(cardId))){
-                const cartItemIndex = cardToSell.findIndex(item => item.card === cardId);
-                cardToSell[cartItemIndex] = { ...cardToSell[cartItemIndex], nbToSell: cardToSell[cartItemIndex].nbToSell + 1 }
-            }else{
-                setCardToSell(cardToSell => [...cardToSell,{card: cardId,nbToSell:1}]);
+            if(cardToSell[cartItemIndex].nbToSell + 1 <= cardNb){
+                if(cardToSell.find((card) => card.card.includes(cardId))){
+                    cardToSell[cartItemIndex] = { ...cardToSell[cartItemIndex], nbToSell: cardToSell[cartItemIndex].nbToSell + 1 }
+                }else{
+                    setCardToSell(cardToSell => [...cardToSell,{card: cardId,nbToSell:1}]);
+                }
             }
         }else{
             setCardToSell(cardToSell => [...cardToSell,{card: cardId,nbToSell:1}]);
