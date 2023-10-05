d5f715f897f5d46addc188aab5a99c001ab68d88
pierre schuvey <chromatykretro@gmail.com>
2023-10-05 17:24:14 +0200

    sell

--- src/component/myCardSell.js
+++ src/component/myCardSell.js
@@ -236,7 +236,6 @@ function MyCardSell(props) {
             setMyCardsId(myCardsId => [...myCardsId,val.card]);
         })
     }, [myCards]);
-    console.log(rarities.find((stadeU) => stadeU.rarity.includes("Common")));
     return (
         <>
 
