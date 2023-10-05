3f0c84aa93f6e992fa8f45bf1172e9a2cfa22b00
pierre schuvey <chromatykretro@gmail.com>
2023-10-05 16:19:27 +0200

    sell

--- src/component/myCardSell.js
+++ src/component/myCardSell.js
@@ -163,6 +163,7 @@ function MyCardSell(props) {
                         }
                     ).then(function(response){
                         setIsOpen(false);
+                        setCardToSell([]);
                     })
                 })
         })
