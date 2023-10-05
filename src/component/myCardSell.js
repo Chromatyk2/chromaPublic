3daa14f5a6bec3cec3d48ec32912299d3e9928d4
pierre schuvey <chromatykretro@gmail.com>
2023-10-05 17:13:31 +0200

    sell

--- src/component/myCardSell.js
+++ src/component/myCardSell.js
@@ -262,7 +262,9 @@ function MyCardSell(props) {
                             rarities &&
                                 myCards.map((val, key) => {
                                     var rarityC = items.data.find((myCard) => myCard.id.includes(val.card)).rarity;
-                                    var stadeC = rarities.find((stadeU) => stadeU.rarity.includes(rarityC));
+                                    if(rarities.find((stadeU) => stadeU.rarity.includes(rarityC) !== "undefined")){
+                                        var stadeC = rarities.find((stadeU) => stadeU.rarity.includes(rarityC)).rarity;
+                                    }
                                         return (
                                             <>
                                                 <button style={customStyles.buttonMyCard} className={"cardBox"}>
