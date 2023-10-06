7916b3d6eeadce7605ea96c1591243141c893977
pierre schuvey <chromatykretro@gmail.com>
2023-10-06 15:49:41 +0200

    sell

--- src/component/myCardSet.js
+++ src/component/myCardSet.js
@@ -158,7 +158,7 @@ function MyCardsSet(props) {
                     </div>
                     <div id={"cardsContainer"}>
                         {items &&
-                            items.sort((a, b) => a.name.localeCompare(b.name).map((val, key) => {
+                            items.sort((a, b) => a.name.localeCompare(b.name)).map((val, key) => {
                                 if (myCardsId.includes(val.id)) {
                                     if(val.rarity != "Common" && val.rarity != "Uncommon" && typeof stadeB !== "undefined"){
                                         var stadeC = rarities.find((uc) => uc.rarity.includes(val.rarity)).stade;
