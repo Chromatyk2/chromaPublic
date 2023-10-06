1a8187f53234c7a67d41e87315638ab3d4c315d6
pierre schuvey <chromatykretro@gmail.com>
2023-10-06 13:45:31 +0200

    sell

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -34,7 +34,7 @@ function OpeningCards(props) {
     useEffect(() => {
         if (tenCards.length < 11) {
             if(tenCards.length < 7){
-                const commonArray = props.items.filter(item => item.rarity == 'Common' || item.supertype === "Energy");
+                const commonArray = props.items.filter(item => item.supertype === "Energy");
                 const randomCommon = commonArray[Math.floor(Math.random() * commonArray.length)];
                 Axios.post('/api/addCard',
                     {
