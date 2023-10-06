5dad5fb6c6b0113ce1fb886c0f914567f7311f6a
pierre schuvey <chromatykretro@gmail.com>
2023-10-06 12:32:14 +0200

    sell

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -12,6 +12,7 @@ function NavBar(props) {
   const [count, setCount] = useState(0);
   const [stream, setStream] = useState(null);
   const pseudo = props.cookies.user.data[0].login;
+  const [expanded, setExpanded] = useState(false);
   useEffect(() => {
       Axios
         .get("/api/getCountProposition/"+pseudo)
@@ -20,27 +21,43 @@ function NavBar(props) {
       })
   }, [])
   return (
-    <Navbar expand="lg">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link className="navLink" to="/">Accueil</Link>
-              <Link className="navLink" to="/29ct92B3ZrvxGT">TCG</Link>
-            {pseudo == "chromatyk" &&
-              <>
-                  <Link className="navLink" to="/pokedex">Pokedex</Link>
-                  <Link className="navLink" to="/leaderboard">Classement</Link>
-                  <Link className="navLink myTradesLink" to="/myTrades">Mes Echanges {count > 0 && <span className="myCountProposition">{count}</span>}</Link>
-                  <Link className="navLink" to="/tradePlace">Place aux echanges</Link>
-                <Link className="navLink" to="/aNu5YwZ5X75m5j">Note</Link>
-                <Link className="navLink" to="/29ct92B3ZrvxGS">NostalPick</Link>
-              </>
-            }
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+
+      <Navbar expanded={expanded} bg="light" expand="lg">
+          <Container fluid>
+              <Navbar.Brand>My Expenses</Navbar.Brand>
+              <Navbar.Toggle
+                  aria-controls="navbarScroll"
+                  onClick={() => setExpanded(!expanded)}
+              />
+              <Navbar.Collapse id="navbarScroll">
+                  <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+                     <Link onClick={() => setExpanded(false)} className="navLink" to="/">Accueil</Link>
+                      <Link onClick={() => setExpanded(false)} className="navLink" to="/29ct92B3ZrvxGT">TCG</Link>
+                  </Nav>
+              </Navbar.Collapse>
+          </Container>
+      </Navbar>
+    // <Navbar expand="lg">
+    //   <Container>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav className="me-auto">
+    //         <Link className="navLink" to="/">Accueil</Link>
+    //           <Link className="navLink" to="/29ct92B3ZrvxGT">TCG</Link>
+    //         {pseudo == "chromatyk" &&
+    //           <>
+    //               <Link className="navLink" to="/pokedex">Pokedex</Link>
+    //               <Link className="navLink" to="/leaderboard">Classement</Link>
+    //               <Link className="navLink myTradesLink" to="/myTrades">Mes Echanges {count > 0 && <span className="myCountProposition">{count}</span>}</Link>
+    //               <Link className="navLink" to="/tradePlace">Place aux echanges</Link>
+    //             <Link className="navLink" to="/aNu5YwZ5X75m5j">Note</Link>
+    //             <Link className="navLink" to="/29ct92B3ZrvxGS">NostalPick</Link>
+    //           </>
+    //         }
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
   );
 }
 
