9dc71766d1fab7fcf6b92878d0faa6e68afcfb2e
pierre schuvey <chromatykretro@gmail.com>
2023-10-10 13:48:09 +0200

    b

--- src/component/cardsShop.js
+++ src/component/cardsShop.js
@@ -82,118 +82,136 @@ function CardsShop(props) {
                                     setPoints(response.data[0].points);
                                 }).then(
                                 (result) => {
-                                    Axios.post('/api/addBooster',
-                                        {
-                                            pseudo:props.user,
-                                            booster:idBooster
-                                        }).then(
-                                        (result) => {
-                                            setLoading(false);
-                                        }
-                                    )
+                                    Axios
+                                        .get("/api/getMyBoostersByOne/" + props.user + "/" + idBooster)
+                                        .then(function (response) {
+                                            if (reponse.data === null) {
+                                                Axios.post('/api/addBooster',
+                                                    {
+                                                        pseudo: props.user,
+                                                        booster: idBooster,
+                                                        nbBooster:nbPick
+                                                    }).then(
+                                                    (result) => {
+                                                        setLoading(false);
+                                                    })
+                                            } else {
+                                                Axios.post('/api/updateBooster',
+                                                    {
+                                                        pseudo: props.user,
+                                                        booster: idBooster,
+                                                        nbBooster:nbPick
+                                                    }).then(
+                                                    (result) => {
+                                                        setLoading(false);
+                                                    })
+                                            }
+                                        })
                                 }
                             )
                         }
                     )
                 }
-            })
-    }
-    function registerCards(e) {
-        return Axios.post('/api/registerCards',
-            {
-                pseudo:e.target.value
+            )
             }
-        ).then(
-            (result) => {
-                Axios
-                    .get("/api/getCardsPoint/"+e.target.value)
-                    .then(function(response){
-                        setPoints(response.data[0].points);
-                    })
-            }
-        )
-    }
-
-    function selectGen(e) {
-        if(e.target.value == "all"){
+    })
+}
+function registerCards(e) {
+    return Axios.post('/api/registerCards',
+        {
+            pseudo:e.target.value
+        }
+    ).then(
+        (result) => {
             Axios
-                .get("/api/getBoostersList")
+                .get("/api/getCardsPoint/"+e.target.value)
                 .then(function(response){
-                    setItems(response.data);
-                })
-        }else{
-            Axios
-                .get("/api/getBoostersListByGen/"+ e.target.value)
-                .then(function(response){
-                    setItems(response.data);
+                    setPoints(response.data[0].points);
                 })
         }
+    )
+}
+
+function selectGen(e) {
+    if(e.target.value == "all"){
+        Axios
+            .get("/api/getBoostersList")
+            .then(function(response){
+                setItems(response.data);
+            })
+    }else{
+        Axios
+            .get("/api/getBoostersListByGen/"+ e.target.value)
+            .then(function(response){
+                setItems(response.data);
+            })
     }
-    return (
-        <>
-                <div>
-                    {points &&
-                    points == -1 ?
-                        <div className="myPointsDisplay">
-                        </div>
+}
+return (
+    <>
+        <div>
+            {points &&
+            points == -1 ?
+                <div className="myPointsDisplay">
+                </div>
+                :
+                <div className="myPointsDisplay">
+                    <p>Points Boutique : {points}</p>
+                </div>
+            }
+        </div>
+        <select className={"selectGen"} onChange={selectGen} name="pets" id="pet-select">
+            <option value="all">All Gen</option>
+            <option value="1">Gen 1</option>
+            <option value="2">Gen 2</option>
+            <option value="3">Gen 3</option>
+            <option value="4">Gen 4</option>
+            <option value="5">Gen 5</option>
+            <option value="6">Gen 6</option>
+            <option value="7">Gen 7</option>
+            <option value="8">Gen 8</option>
+            <option value="9">Gen 9</option>
+        </select>
+        <div id={"cardsContainer"}>
+            {items &&
+                <div className="uniqueTradeContainer">
+                    <p className="pokemonNameTrade">Booster Aléatoire</p>
+                    <div className={"containerImgBooster"}>
+                        <img className="fit-picture" src={"/images/random.png"} alt="Grapefruit slice atop a pile of other slices"/>
+                    </div>
+                    <p className="pokemonNameTrade">500 Points Boutique</p>
+                    {points > 499 ?
+                        loading === false ?
+                            <button value={items[Math.floor(Math.random() * items.length)].name} onClick={buyBoosterRandom} className="guessTradeButton">Acheter</button>
+                            :
+                            <button className="guessTradeButton">Chargement</button>
                         :
-                        <div className="myPointsDisplay">
-                            <p>Points Boutique : {points}</p>
-                        </div>
+                        <button className="guessTradeButton">Card Points manquants</button>
                     }
                 </div>
-                <select className={"selectGen"} onChange={selectGen} name="pets" id="pet-select">
-                    <option value="all">All Gen</option>
-                    <option value="1">Gen 1</option>
-                    <option value="2">Gen 2</option>
-                    <option value="3">Gen 3</option>
-                    <option value="4">Gen 4</option>
-                    <option value="5">Gen 5</option>
-                    <option value="6">Gen 6</option>
-                    <option value="7">Gen 7</option>
-                    <option value="8">Gen 8</option>
-                    <option value="9">Gen 9</option>
-                </select>
-                <div id={"cardsContainer"}>
-                    {items &&
+            }
+            {items &&
+                items.map((val, key) => {
+                    return(
                         <div className="uniqueTradeContainer">
-                            <p className="pokemonNameTrade">Booster Aléatoire</p>
                             <div className={"containerImgBooster"}>
-                                <img className="fit-picture" src={"/images/random.png"} alt="Grapefruit slice atop a pile of other slices"/>
+                                <img className="fit-picture" src={"https://images.pokemontcg.io/" + val.name + "/logo.png"} alt="Grapefruit slice atop a pile of other slices"/>
                             </div>
-                            <p className="pokemonNameTrade">500 Points Boutique</p>
-                            {points > 499 ?
+                            <p className="pokemonNameTrade">1000 Points Boutique</p>
+                            {points > 999 ?
                                 loading === false ?
-                                    <button value={items[Math.floor(Math.random() * items.length)].name} onClick={buyBoosterRandom} className="guessTradeButton">Acheter</button>
+                                    <button value={val.name} onClick={buyBooster} className="guessTradeButton">Acheter</button>
                                     :
                                     <button className="guessTradeButton">Chargement</button>
                                 :
                                 <button className="guessTradeButton">Card Points manquants</button>
                             }
                         </div>
-                    }
-                    {items &&
-                        items.map((val, key) => {
-                            return(
-                                <div className="uniqueTradeContainer">
-                                    <div className={"containerImgBooster"}>
-                                        <img className="fit-picture" src={"https://images.pokemontcg.io/" + val.name + "/logo.png"} alt="Grapefruit slice atop a pile of other slices"/>
-                                    </div>
-                                    <p className="pokemonNameTrade">1000 Points Boutique</p>
-                                    {points > 999 ?
-                                        loading === false ?
-                                            <button value={val.name} onClick={buyBooster} className="guessTradeButton">Acheter</button>
-                                            :
-                                            <button className="guessTradeButton">Chargement</button>
-                                        :
-                                        <button className="guessTradeButton">Card Points manquants</button>
-                                    }
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-        </>
-    )
+                    )
+                })
+            }
+        </div>
+    </>
+)
 }
 export default CardsShop
