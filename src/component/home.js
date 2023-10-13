e3703442175c00ada2e54468a398d6ca1e67ccf6
pierre schuvey <chromatykretro@gmail.com>
2023-10-13 22:05:08 +0200

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -10,13 +10,9 @@ function HomePage(props) {
     useEffect(() => {
         const handler = e => {
             e.preventDefault();
-            console.log("we are being triggered :D");
             setSupportsPWA(true);
             setPromptInstall(e);
         };
-        window.addEventListener("beforeinstallprompt", handler);
-
-        return () => window.removeEventListener("transitionend", handler);
     }, []);
 
     function onClick(evt) {
