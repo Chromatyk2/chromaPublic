c9adc6565396c84531a41b48d5efe81a61830502
pierre schuvey <chromatykretro@gmail.com>
2023-10-21 22:29:14 +0200

    obs

--- src/component/nostalPickv2.js
+++ src/component/nostalPickv2.js
@@ -11,7 +11,7 @@ function NostalPickV2(props) {
     async function getObs() {
         const obs = new OBSWebSocket();
         await obs.connect('ws://127.0.0.1:4455', '96W4bUiEf55vFs');
-        const data = await obs.call('GetSourceActive');
+        const data = await obs.call('GetSourceActive', {sourceName: 'Logo Movement'});
         console.log(data);
     };
     getObs();
