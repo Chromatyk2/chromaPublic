4f8ea0c24f10025e0a332c8105236ead3c44a1e5
pierre schuvey <chromatykretro@gmail.com>
2023-11-03 16:29:10 +0100

    fix

--- src/component/Succes.js
+++ src/component/Succes.js
@@ -89,7 +89,7 @@ function Succes(props) {
                             return(
                                 <div className={"containerBadgeSucces"}>
                                     <img src={"/images/25succes.png"} className={"backSucces"} />
-                                    <div style={{backgroundImage:`url(/images/${val.booster}.png`}} className={"imgContainerSucces"}>
+                                    <div style={{height:'50px', backgroundImage:`url(/${val.booster}.png`}} className={"imgContainerSucces"}>
                                     </div>
                                 </div>
                             )
