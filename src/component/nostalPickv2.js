ce5226a081ed10616e084eafb5e2253b6fe84d0a
pierre schuvey <chromatykretro@gmail.com>
2024-01-08 15:33:57 +0100

    fix

--- src/component/nostalPickv2.js
+++ src/component/nostalPickv2.js
@@ -11,9 +11,9 @@ function NostalPickV2(props) {
     const [modalIsOpen, setIsOpen] = React.useState(false);
     const [pickConsole, setPickConsole] = React.useState("null");
     const [randomConsoles, setRandomConsoles] = React.useState(null);
-    var numbers = [1,2,3,4,5,6,7,8,9];
+    var numbers = [1,2,3,4,5,6,7,8,9,10];
     useEffect(() => {
-        var consoles = ["GBA","GBC","MEGADRIVE","N64","NDS","NGC","PS1","PSP","SNES"];
+        var consoles = ["GBA","GBC","MEGADRIVE","N64","NDS","NGC","PS1","PSP","SNES","NES","MASTERSYSTEM"];
         setRandomConsoles(consoles.sort(() => Math.random() - 0.5));
     }, []);
     const customStyles = {
