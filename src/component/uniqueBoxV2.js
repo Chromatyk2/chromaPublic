aab75d63b34c081ed191325240683dfd5c4ce8de
pierre schuvey <chromatykretro@gmail.com>
2024-01-09 13:54:30 +0100

    fix

--- src/component/uniqueBoxV2.js
+++ src/component/uniqueBoxV2.js
@@ -59,13 +59,10 @@ function UniqueBoxV2(props) {
             }.bind(this), 1000)
             var epic = Math.floor(Math.random() * 100);
             if(epic > 100) {
-                setTimeout(function() {
-                    document.getElementById("box").classList.toggle("spinBox");
-                }.bind(this), 3000)
                 var legendary = Math.floor(Math.random() * 100);
-                if(legendary > 100){
+                if(legendary < 100){
                     var ultra = Math.floor(Math.random() * 100);
-                    if(ultra > 100){
+                    if(ultra < 100){
                         setTimeout(function() {
                             setTypeBox("rare");
                         }.bind(this), 1000)
@@ -80,14 +77,14 @@ function UniqueBoxV2(props) {
                         }.bind(this), 10000)
                         setTimeout(function() {
                             document.getElementById("box").classList.toggle("spinBox");
-                        }.bind(this), 9000)
+                        }.bind(this), 6000)
                         setTimeout(function() {
                             setTypeBox("ultraOpen");
                             setFinalState("ultra");
                             $('audio#karateka')[0].pause()
                             $('audio#karateka')[0].currentTime = 0
                             $('audio#rainbowWin')[0].play()
-                        }.bind(this), 11000)
+                        }.bind(this), 7000)
                     }else{
                         setTimeout(function() {
                             setTypeBox("rare");
@@ -100,7 +97,7 @@ function UniqueBoxV2(props) {
                         }.bind(this), 5000)
                         setTimeout(function() {
                             document.getElementById("box").classList.toggle("spinBox");
-                        }.bind(this), 7000)
+                        }.bind(this), 6000)
                         setTimeout(function() {
                             document.getElementById("box").classList.toggle("spinBox");
                             setTypeBox("legendaryOpen");
@@ -108,7 +105,7 @@ function UniqueBoxV2(props) {
                             $('audio#orangeWin')[0].play()
                             $('audio#karateka')[0].pause()
                             $('audio#karateka')[0].currentTime = 0
-                        }.bind(this), 9000)
+                        }.bind(this), 7000)
                     }
                 }else{
                     setTimeout(function() {
@@ -119,7 +116,7 @@ function UniqueBoxV2(props) {
                     }.bind(this), 3000)
                     setTimeout(function() {
                         document.getElementById("box").classList.toggle("spinBox");
-                    }.bind(this), 5000)
+                    }.bind(this), 1000)
                     setTimeout(function() {
                         document.getElementById("box").classList.toggle("spinBox");
                         setTypeBox("epicOpen");
@@ -127,7 +124,7 @@ function UniqueBoxV2(props) {
                         $('audio#karateka')[0].pause()
                         $('audio#karateka')[0].currentTime = 0
                         $('audio#blueWin')[0].play()
-                    }.bind(this), 7000)
+                    }.bind(this), 4000)
                 }
             }else {
                 setTimeout(function() {
