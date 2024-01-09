68c0940be8465d261f4e2276505676690e5ec528
pierre schuvey <chromatykretro@gmail.com>
2024-01-09 14:45:13 +0100

    fix

--- src/component/uniqueBoxV2.js
+++ src/component/uniqueBoxV2.js
@@ -66,7 +66,13 @@ function UniqueBoxV2(props) {
                     document.getElementById("box").classList.toggle("spinBox");
                 }.bind(this), 5000)
                 var legendary = Math.floor(Math.random() * 100);
-                if(legendary > 100){
+                if(legendary < 100){
+                    setTimeout(function() {
+                        document.getElementById("box").classList.toggle("spinBox");
+                    }.bind(this), 9000)
+                    setTimeout(function() {
+                        document.getElementById("box").classList.toggle("spinBox");
+                    }.bind(this), 11000)
                     var ultra = Math.floor(Math.random() * 100);
                     if(ultra > 100){
                         setTimeout(function() {
@@ -80,17 +86,14 @@ function UniqueBoxV2(props) {
                         }.bind(this), 5000)
                         setTimeout(function() {
                             setTypeBox("ultra");
-                        }.bind(this), 10000)
-                        setTimeout(function() {
-                            document.getElementById("box").classList.toggle("spinBox");
-                        }.bind(this), 9000)
+                        }.bind(this), 12000)
                         setTimeout(function() {
                             setTypeBox("ultraOpen");
                             setFinalState("ultra");
                             $('audio#karateka')[0].pause()
                             $('audio#karateka')[0].currentTime = 0
                             $('audio#rainbowWin')[0].play()
-                        }.bind(this), 11000)
+                        }.bind(this), 12000)
                     }else{
                         setTimeout(function() {
                             setTypeBox("rare");
@@ -100,10 +103,7 @@ function UniqueBoxV2(props) {
                         }.bind(this), 3000)
                         setTimeout(function() {
                             setTypeBox("legendary");
-                        }.bind(this), 5000)
-                        setTimeout(function() {
-                            document.getElementById("box").classList.toggle("spinBox");
-                        }.bind(this), 7000)
+                        }.bind(this), 12000)
                         setTimeout(function() {
                             document.getElementById("box").classList.toggle("spinBox");
                             setTypeBox("legendaryOpen");
@@ -111,7 +111,7 @@ function UniqueBoxV2(props) {
                             $('audio#orangeWin')[0].play()
                             $('audio#karateka')[0].pause()
                             $('audio#karateka')[0].currentTime = 0
-                        }.bind(this), 9000)
+                        }.bind(this), 12000)
                     }
                 }else{
                     setTimeout(function() {
