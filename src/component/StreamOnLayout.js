b7cf43e6945a759aa274028e8eac91737503d7ea
pierre schuvey <chromatykretro@gmail.com>
2024-05-14 12:28:55 +0200

    fix

--- src/component/StreamOnLayout.js
+++ src/component/StreamOnLayout.js
@@ -12,7 +12,7 @@ import {useCookies} from "react-cookie";
 function StreamOnLayout() {
     const [cookies, setCookie] = useCookies();
     const [count, setCount] = useState(0);
-    const [team, setTeam] = useState();
+    const [team, setTeam] = useState([]);
     const [displayStream, setDisplayStream] = useState(true);
     const pseudo = cookies.user.data[0].login;
     useEffect(() => {
@@ -28,7 +28,7 @@ function StreamOnLayout() {
             setTeam(response.data);
         })
     }, [])
-    console.log(team);
+    console.log(team.data[0]);
     return (
         <>
             {team &&
