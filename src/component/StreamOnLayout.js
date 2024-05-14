c6a32bc3fd22cc32e76f2b86555ce9b903e216db
pierre schuvey <chromatykretro@gmail.com>
2024-05-14 11:22:53 +0200

    fix

--- src/component/StreamOnLayout.js
+++ src/component/StreamOnLayout.js
@@ -25,7 +25,7 @@ function StreamOnLayout() {
                 }
             }
         ).then(function(response){
-            setTeam(response.data[0]);
+            setTeam(response.data);
         })
     }, [])
     console.log(team)
