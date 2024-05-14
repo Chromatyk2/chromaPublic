cd34c56c770c5324baf8595ae2d0afbfdf9bbe44
pierre schuvey <chromatykretro@gmail.com>
2024-05-15 01:47:28 +0200

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -10,19 +10,21 @@ function HomePage(props) {
         <div className="homeContainer">
             <div className={"homeContent"}>
                 <img style={{width: "35%"}} src={"images/logoSofk.png"}/>
-                <p>
-                    Le Stream On for Kids est un évènement caritatif en ligne se déroulant chaque année à l’occasion de la
-                    Journée Internationale des Enfants Disparus pour collecter des dons en faveur du 116 000 Enfants
-                    Disparus. Ce numéro d’urgence gratuit, disponible 24h/24 et 7j7, accompagne les familles confrontées à
-                    la disparition de leur enfant.
-                </p>
-                <br />
-                <p>
-                    Créé à l’initiative de Vaykhin en 2021, le Stream On for Kids se déroule sur Twitch, une plateforme de
-                    vidéos en direct sur laquelle des streamers diffusent du contenu en interagissant avec leurs
-                    spectateurs. L’événement a permis de collecter près de 30 000 € en faveur du numéro d’urgence lors de
-                    ses trois premières éditions et permet également d’accroitre la notoriété du numéro 116 000 auprès du public.
-                </p>
+                <div>
+                    <p className={"paragraphHome"}>
+                        Le <span>Stream On for Kids</span> est un évènement caritatif en ligne se déroulant chaque année à l’occasion
+                        de la <span>Journée Internationale des Enfants Disparus</span> pour collecter des dons en faveur du <span>116 000 Enfants
+                        Disparus</span>. Ce numéro d’urgence gratuit, disponible <span>24h/24 et 7j7</span>, accompagne les familles
+                        confrontées à la disparition de leur enfant.
+                    </p>
+                    <p className={"paragraphHome"}>
+                        Créé à l’initiative de <span>Vaykhin</span> en 2021, le <span>Stream On for Kids</span> se déroule sur Twitch, une
+                        plateforme de vidéos en direct sur laquelle des streamers diffusent du contenu en interagissant avec leurs
+                        spectateurs. L’événement a permis de collecter près de <span>30 000 €</span> en faveur du numéro d’urgence
+                        lors de ses trois premières éditions et permet également d’accroitre la notoriété du numéro <span>116 000</span>
+                        auprès du public.
+                    </p>
+                </div>
             </div>
             <a className="socialLink" target='_blank' href="https://twitch.tv/chromatyk">Faire un Don</a>
         </div>
