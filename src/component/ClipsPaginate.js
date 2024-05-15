a63ae5ddfa53c7783e0b9ffec6fa7d1862c78758
pierre schuvey <chromatykretro@gmail.com>
2024-05-15 16:00:34 +0200

    fix

--- src/component/ClipsPaginate.js
+++ src/component/ClipsPaginate.js
@@ -16,7 +16,7 @@ function Items(props) {
                             <>
                                 <Link className="clipsLink navLink" to={val.url}>
                                     <img className={"imgClip"} src={val.thumbnail_url}/>
-                                    <p className={"titleClip"}>{val.title.substring(0, 20) + "..."}</p>
+                                    <p className={"titleClip"}>{val.title.substring(0, 25) + val.title.length > 25 && "..."}</p>
                                     <div className={"infoClipContainer"}>
                                         <p className={"viewsClips"}>{val.view_count} vues</p>
                                         <p className={"dateClip"}>{moment(val.created_at).utc().format('DD/MM/YYYY')}</p>
