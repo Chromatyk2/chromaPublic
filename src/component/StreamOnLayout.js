958f136fee3153e3d9c18129c1eb16f7273aed62
pierre schuvey <chromatykretro@gmail.com>
2024-05-16 16:11:08 +0200

    fix

--- src/component/StreamOnLayout.js
+++ src/component/StreamOnLayout.js
@@ -16,8 +16,10 @@ function StreamOnLayout() {
         Axios.get(
             'https://streamlabscharity.com/api/v1/teams/643437249115068091'
         ).then(function (response) {
-            console.log(response);
-            setCharityTeam(oldArrayOn => [...oldArrayOn, {infos: response.data.members[1]}]);
+            console.log(response.data.members);
+            response.data.members.map((val, key) => {
+                setCharityTeam(oldArrayOn => [...oldArrayOn, {infos: val.user}]);
+            })
         })
     }, []);
     useEffect(() => {
