ba3c527e374ee5b01318f5b2f0b8eeac3b6ed27b
pierre schuvey <chromatykretro@gmail.com>
2024-05-16 12:45:39 +0200

    fix

--- src/component/ClipsLayout.js
+++ src/component/ClipsLayout.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import Axios from 'axios'
 import {useCookies} from "react-cookie";
 import ClipsPaginate from "./ClipsPaginate";
+import UniqueStreamer from "./uniqueStreamer";
+import UniqueStreamerMozaique from "./UniqueStreamerMozaique";
 
 function ClipsLayout() {
     const [cookies, setCookie] = useCookies();
@@ -35,7 +37,64 @@ function ClipsLayout() {
             })
         })
     }, [])
-
+    useEffect(() => {
+        Axios.get(
+            'https://api.twitch.tv/helix/teams?name=streamon',
+            {
+                headers: {
+                    'Authorization': `Bearer ${cookies.token.access_token}`,
+                    'Client-Id': process.env.REACT_APP_CLIENT_ID
+                }
+            }
+        ).then(function (response) {
+            setTeam(response.data.data[0].users);
+            response.data.data[0].users.map((val, key) => {
+                Axios.get(
+                    'https://api.twitch.tv/helix/streams?user_login=' + val.user_name,
+                    {
+                        headers: {
+                            'Authorization': `Bearer ${cookies.token.access_token}`,
+                            'Client-Id': process.env.REACT_APP_CLIENT_ID
+                        }
+                    }
+                ).then(function (response) {
+                    if (response.data.data.length > 0) {
+                        setOnStream(oldArrayOn => [...oldArrayOn, {infos: response.data.data}]);
+                    } else if (response.data.data.length < 1) {
+                        setOffStream(oldArrayOff => [...oldArrayOff, val.user_name]);
+                    }
+                })
+            })
+        })
+    }, [])
+    return (
+        <div className={"containerStream"}>
+            <div className={"streamersList"}>
+                <p className={"streamTitle"}>Streameur.euses</p>
+                <hr style={{width: "50%", display: "block", margin: "auto", border: "1px solid #f7bb3e"}}/>
+                {orderedOnStream.length > 0 &&
+                    onStream.map((val, key) => {
+                        return (
+                            <UniqueStreamer change={handleDataFromChild} onStream={true} streamer={val}/>
+                        )
+                    })
+                }
+                {offStream.length > 0 &&
+                    offStream.map((val, key) => {
+                        return (
+                            <UniqueStreamer change={handleDataFromChild} onStream={false} streamer={val}/>
+                        )
+                    })
+                }
+            </div>
+            {clips.length > 0 &&
+                <ClipsPaginate
+                    itemsPerPage={32}
+                    items={clips}
+                />
+            }
+        </div>
+    )
     return (
         <>
             {clips.length > 0 &&
