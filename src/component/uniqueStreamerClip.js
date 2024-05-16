fb461bcf12d3af123e2a904b0f6c2d40f9f806de
pierre schuvey <chromatykretro@gmail.com>
2024-05-16 13:21:14 +0200

    fix

--- src/component/uniqueStreamerClip.js
+++ src/component/uniqueStreamerClip.js
@@ -25,6 +25,7 @@ function UniqueStreamerClip(props) {
         })
     }, [])
     function changeStream(e) {
+        console.log(e);
         e.preventDefault();
         props.change(e.target.value);
     }
