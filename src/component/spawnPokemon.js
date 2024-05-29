276e1652ccdf73c73374df5abd0ab4500c60627e
pierre schuvey <chromatykretro@gmail.com>
2024-05-29 14:53:57 +0200

    fix

--- src/component/spawnPokemon.js
+++ src/component/spawnPokemon.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../App.css'
+import {useParams} from "react-router-dom";
 function SpawnPokemon(props) {
+    const pseudo = props.cookies.user.data[0].login;
+    const [pokemon, setPokemon] = useState([]);
+    const [name, setName] = useState([]);
+    const [captures, setCaptures] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const { id } = useParams()
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon/"+id)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setPokemon(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
     return (
         <>
-            <div className="pokeball"></div>
+            {pokemon &&
+                <img className="imgPokemonPage" src={pokemon.sprites.other.home.front_default}></img>
+                <div className="pokeball"></div>
+            }
         </>
     );
 }
