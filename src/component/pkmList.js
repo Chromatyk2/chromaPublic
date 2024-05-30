e6a2b18c7fd1c80b95298af9375ab79fc6695eb2
pierre schuvey <chromatykretro@gmail.com>
2024-05-30 17:31:08 +0200

    v2

--- src/component/pkmList.js
+++ src/component/pkmList.js
@@ -4,16 +4,8 @@ import Axios from 'axios'
 import Pagination from './paginate.js';
 import '../App.css'
 function PkmList(props) {
-  const pkmList = props.list;
-  const shinys = pkmList.filter(item => item.shiny == 1);
-  const nbShiny = shinys.length;
-  const nbTotal = pkmList.length;
     return (
       <>
-        <div className="stats">
-          <p className="labelStats">Shiny<br/><span className="valueStats">{nbShiny}</span></p>
-          <p className="labelStats">Total<br/><span className="valueStats">{nbTotal}</span></p>
-        </div>
           <Pagination
             itemsPerPage={30}
             items={props.list}
