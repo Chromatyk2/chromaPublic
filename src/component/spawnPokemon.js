18467afc58fc3433e4a272961ae082e25e46287a
pierre schuvey <chromatykretro@gmail.com>
2024-05-30 10:06:54 +0200

    fix

--- src/component/spawnPokemon.js
+++ src/component/spawnPokemon.js
@@ -1,27 +1,48 @@
 import React, {useEffect, useState} from 'react';
 import '../App.css'
-import {useParams} from "react-router-dom";
 function SpawnPokemon(props) {
     const pseudo = props.cookies.user.data[0].login;
     const [pokemon, setPokemon] = useState([])
     const [balls, setBalls] = useState(['poke','great','ultra','safari','premier','sport','net','dive','nest','repeat','timer','luxury','dusk','heal','quick','fast','level','lure','heavy','love','friend','moon','park','dream','beast','cherish','master']);
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(true);
+    const [idPkm, setIdPkm] = useState(null);
     useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon/"+ Math.floor((Math.random() * 1025) + 1))
+        setIdPkm(Math.floor((Math.random() * 1025) + 1));
+        fetch("https://pokeapi.co/api/v2/pokemon-species/"+idPkm)
             .then(res => res.json())
             .then(
                 (result) => {
-                    setPokemon(result);
-                    if(Math.floor((Math.random() * 100) + 1) == 1){
-                        setIsLoaded(false);
-                        let root = document.querySelector(':root');
-                        root.style.setProperty('--backGgroundImage', 'url('+result.sprites.front_shiny+')');
-                    }else{
-                        setIsLoaded(false);
-                        let root = document.querySelector(':root');
-                        root.style.setProperty('--backGgroundImage', 'url('+result.sprites.front_default+')');
+                    switch (result.is_legendary){
+                        case true:
+                            console.log("légendaire");
+                            break;
+                        default :
+                            console.log("pas légendaire")
                     }
+                    // setPokemon(result);
+                    // fetch("https://pokeapi.co/api/v2/pokemon-species/"+idPkm)
+                    //     .then(res => res.json())
+                    //     .then(
+                    //         (result) => {
+                    //             if(Math.floor((Math.random() * 100) + 1) == 1){
+                    //
+                    //             }
+                    //             if(Math.floor((Math.random() * 100) + 1) == 1){
+                    //                 setIsLoaded(false);
+                    //                 let root = document.querySelector(':root');
+                    //                 root.style.setProperty('--backGgroundImage', 'url('+pokemon.sprites.front_shiny+')');
+                    //             }else{
+                    //                 setIsLoaded(false);
+                    //                 let root = document.querySelector(':root');
+                    //                 root.style.setProperty('--backGgroundImage', 'url('+pokemon.sprites.front_default+')');
+                    //             }
+                    //         },
+                    //         (error) => {
+                    //             setIsLoaded(true);
+                    //             setError(error);
+                    //         }
+                    //     )
                 },
                 (error) => {
                     setIsLoaded(true);
@@ -29,15 +50,6 @@ function SpawnPokemon(props) {
                 }
             )
     }, [])
-
-    const customStyles = {
-        '.mon':{
-            '&::before': {
-                backgroundImage : 'url("https://img.pokemondb.net/sprites/black-white/anim/normal/dratini.gif")'
-            }
-        }
-    }
-    console.log(balls[Math.floor(Math.random() * balls.length)]);
     return (
         <>
             {isLoaded === false &&
