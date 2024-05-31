da57346b5a832d3be881300b25d0dbc01207f2be
pierre schuvey <chromatykretro@gmail.com>
2024-05-31 09:55:02 +0200

    fix

--- src/component/home.js
+++ src/component/home.js
@@ -7,15 +7,17 @@ import OnStream from "./onStream";
 function HomePage(props) {
   return (
     <>
-      <OnStream />
-      <p style={{textAlign:"center"}}>Bienvenue, {props.cookies.user.data[0].login}</p>
-      <div className="socialContainer">
-        <p className="myNetworks">Mes reseaux</p>
-        <a className="socialLink" target='_blank' href="https://discord.gg/8V6fyQdSCG"><i class="fa-brands fa-discord"></i>Discord</a>
-        <a className="socialLink" target='_blank' href="https://twitch.tv/chromatyk"><i class="fa-brands fa-twitch"></i>Twitch</a>
-        <a className="socialLink" target='_blank' href="https://twitter.com/Chromatyk_"><i class="fa-brands fa-twitter"></i>Twitter</a>
-        <a className="socialLink" target='_blank' href="https://www.instagram.com/chromatyk_/"><i class="fa-brands fa-instagram"></i>Instagram</a>
-      </div>
+        <div>
+            <OnStream />
+            <p style={{textAlign:"center"}}>Bienvenue, {props.cookies.user.data[0].login}</p>
+            <div className="socialContainer">
+                <p className="myNetworks">Mes reseaux</p>
+                <a className="socialLink" target='_blank' href="https://discord.gg/8V6fyQdSCG"><i class="fa-brands fa-discord"></i>Discord</a>
+                <a className="socialLink" target='_blank' href="https://twitch.tv/chromatyk"><i class="fa-brands fa-twitch"></i>Twitch</a>
+                <a className="socialLink" target='_blank' href="https://twitter.com/Chromatyk_"><i class="fa-brands fa-twitter"></i>Twitter</a>
+                <a className="socialLink" target='_blank' href="https://www.instagram.com/chromatyk_/"><i class="fa-brands fa-instagram"></i>Instagram</a>
+            </div>
+        </div>
     </>
   )
 }
