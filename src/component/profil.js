029b6fd184764d95222520387056d424e92acb3a
pierre schuvey <chromatykretro@gmail.com>
2024-05-31 15:16:06 +0200

    fix

--- src/component/profil.js
+++ src/component/profil.js
@@ -17,7 +17,7 @@ function Profil(props) {
                 width: '75%',
                 backgroundColor: '#90e5b5',
                 position: 'relative',
-                zIndex: '-1',
+                zIndex: '1',
                 borderRadius: '50px',
                 margin:'auto',
                 marginBottom: '15px'
