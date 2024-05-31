3e666c22a7c54e94784dfe9724caceb3985a99cb
pierre schuvey <chromatykretro@gmail.com>
2024-05-31 11:19:11 +0200

    fix

--- src/component/otherCaptures.js
+++ src/component/otherCaptures.js
@@ -11,38 +11,10 @@ function OtherCaptures(props) {
 const [trades, setTrades] = useState([]);
 const [cookies, setCookie] = useCookies();
 const pseudo = cookies.user.data[0].login;
-useEffect(() => {
-  Axios
-    .get("/api/getTradesByPokemon/"+props.pkmId)
-    .then(function(response){
-        setTrades(response.data);
-  })
-}, [])
-  if(trades.length > 0){
     return (
-      <>
-        <div className="myCapturesContainer">
-          <p className="titleMyCaptures">Les echanges</p>
-          {trades.map((val, key) => {
-            return val.shiny == 1 ?
-              <div className="myCaptureItem">
-                <p className="myCapturesList shinyPokemonList">{val.pkmName+"(shiny) - "+moment(val.dateCapture).utc().format('DD/MM/YYYY')}</p>
-              </div>
-              :
-              <div  className="myCaptureItem">
-                <p className="myCapturesList">{val.pkmName+" - "+moment(val.dateCapture).utc().format('DD/MM/YYYY')}</p>
-              </div>
-              })
-          }
-        </div>
-      </>
+        <>
+            <p className="titleMyCaptures">Personne ne propose ce pokemon a l'echange</p>
+        </>
     );
-  }else{
-    return (
-      <>
-        <p className="titleMyCaptures">Personne ne propose ce pokemon a l'echange</p>
-      </>
-    );
-  }
 }
 export default OtherCaptures
--- src/component/pokemonPage.js
+++ src/component/pokemonPage.js
@@ -61,7 +61,7 @@ useEffect(() => {
          <div className={"contentContainer"}>
              <div className="pokemonPageContainer">
                 <div>
-                  <img className="imgPokemonPage" src={pokemon.sprites.other.home.front_default}></img>
+                  <img className="imgPokemonPage" src={pokemon.sprites.front_default} onMouseOver={this.src = require(pokemon.sprites.front_shiny)} onMouseOut={this.src = require(pokemon.sprites.front_default)}></img>
                   <p className="numberPokemonPage"># {pokemon.id}</p>
                   <p className="namePokemonPage">{name[4].name}</p>
                   <div className="pokemonTypeContainer">
