af604af961cf579ee94c68fb19406372cc6be222
pierre schuvey <chromatykretro@gmail.com>
2024-06-01 12:17:49 +0200

    fix

--- src/component/profil.js
+++ src/component/profil.js
@@ -255,13 +255,14 @@ function Profil(props) {
             <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel="Example Modal">
                 {modalGoal == "skin" ?
                     <p style={{textAlign: "center"}}>Choisis ton Skin</p>
-                    profil &&
-                    profil[0].box > 0 &&
-                    <button className={"openSkinBox"} onClick={openSkin} style={{backgroundImage: "url(/basic.png)"}}>
-                        <div className="infoPkm">
-                            <div className="infoNbBoxSkin">{profil[0].box}</div>
-                        </div>
-                    </button>
+                    {profil &&
+                        profil[0].box > 0 &&
+                            <button className={"openSkinBox"} onClick={openSkin} style={{backgroundImage: "url(/basic.png)"}}>
+                                <div className="infoPkm">
+                                    <div className="infoNbBoxSkin">{profil[0].box}</div>
+                                </div>
+                            </button>
+                    }
                     <div style={{overflow: "overlay", display: "flex", gap: "10px", flexWrap: "wrap", flexFlow: "row",flexWrap:"wrap"}}>
                         {skins &&
                             skins.map((val, key) => {
