1149ac4c736546859ef53202905760d6e131a6d4
pierre schuvey <chromatykretro@gmail.com>
2024-06-04 14:40:22 +0200

    fix

--- src/component/paginateProfil.js
+++ src/component/paginateProfil.js
@@ -60,8 +60,10 @@ function Items(props) {
                                             className="anchorTooltip uniquePokemonContainerTeam">
                                         </div>
                                     </div>
-                                    <Link className="navLink linkFromNav" to={"/pokedex/"+val.pseudo}><img src={"/images/pokedex.png"}/></Link>
-                                    <Link className="navLink linkFromNav" to={"/profil/"+val.pseudo}><img src={"/images/pokedex.png"}/></Link>
+                                    <div>
+                                        <Link className="navLink linkFromNav" to={"/pokedex/"+val.pseudo}><img src={"/images/pokedex.png"}/></Link>
+                                        <Link className="navLink linkFromNav" to={"/profil/"+val.pseudo}><img src={"/images/card.png"}/></Link>
+                                    </div>
                                 </div>
                             </>
                         )
