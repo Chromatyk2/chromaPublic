9d9de1affdef9a5cafc753786deabcdde99d0bc1
pierre schuvey <chromatykretro@gmail.com>
2024-06-24 18:43:04 +0200

    fix

--- src/component/pedandex.js
+++ src/component/pedandex.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import ReactPaginate from 'react-paginate';
 import Axios from 'axios'
 import Pagination from './paginate.js';
