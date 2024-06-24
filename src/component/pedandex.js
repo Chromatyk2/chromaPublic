f6a2f40ff0a5c073d6c5777a477e26797deff987
pierre schuvey <chromatykretro@gmail.com>
2024-06-24 15:32:05 +0200

    fix

--- src/component/pedandex.js
+++ src/component/pedandex.js
@@ -4,22 +4,31 @@ import Axios from 'axios'
 import Pagination from './paginate.js';
 import '../App.css'
 function Pedandex(props) {
+    const [description, setDescription] = useState(null);
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon-species/"+Math.floor((Math.random() * 1025) + 1))
             .then(res => res.json())
             .then(
                 (result) => {
-                    console.log(result);
                     const name = result.names.find((element) => element.language.name == "fr").name;
-                    console.log(name);
                     const description = result.flavor_text_entries.find((element) => element.language.name == "fr").flavor_text;
-                    console.log(description);
+                    description.split(' ').forEach(word => {                  // loop over every word
+                        const element = document.createElement("span");  // create element "span"
+                        element.innerText = word;                        // set its content to word
+                        element.onclick = () =>                          // onclick handler
+                            element.style.background = '#000';             // action
+                        document.body.appendChild(element);              // add element to dom
+                    });
+                    setDescription(description)
                 })
     }, []);
     return (
         <>
-            <p>Test</p>
+            {description &&
+                <p>{description}</p>
+            }
         </>
     );
 }
+
 export default Pedandex
