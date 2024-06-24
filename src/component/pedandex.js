aaabe3e2e284e3ee624621fced6d175c247b3517
pierre schuvey <chromatykretro@gmail.com>
2024-06-25 01:07:49 +0200

    fix

--- src/component/pedandex.js
+++ src/component/pedandex.js
@@ -26,7 +26,7 @@ function Pedandex(props) {
             var regex = /\b(Basic|Standard|Superior|Deluxe|Private)\b/gi
             setWords(words => [...words,word]);
             const element = document.createElement("span");
-            console.log(word !== "'")
+            console.log(word !== "'" || word !== ",")
             if(word !== "'")
             {// create element "span"
                 element.innerText = word.replace(/[^.]/g,'x');
