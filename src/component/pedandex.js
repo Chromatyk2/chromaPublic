fa766572e3025c84d9214718e0b568c7ed17fda1
pierre schuvey <chromatykretro@gmail.com>
2024-06-24 17:56:03 +0200

    fix

--- src/component/pedandex.js
+++ src/component/pedandex.js
@@ -4,13 +4,12 @@ import Axios from 'axios'
 import Pagination from './paginate.js';
 import '../App.css'
 function Pedandex(props) {
-    const [description, setDescription] = useState(null);
+    const [name, setName] = useState("Wailord");
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon-species/"+Math.floor((Math.random() * 1025) + 1))
             .then(res => res.json())
             .then(
                 (result) => {
-                    const name = "Wailord";
                     const description = "Wailord est un imposant Pokémon marin inspiré de la baleine bleue (et non du cachalot comme le suggère le Pokédex). Son corps fusiforme est à mi-chemin entre un sous-marin, un dirigeable et une torpille géante. La moitié supérieure de son corps est bleue, la moitié inférieure blanche. Il a de petits yeux noirs, une large bouche, quatre taches blanches le long de son dos et une gorge striée de rainures. Il se meut avec deux paires de nageoires latérales, une paire de nageoires pelviennes et une nageoire caudale horizontale.";
                     var id = 1;
                     var div = document.getElementById("descriptionPedandex");
@@ -26,8 +25,9 @@ function Pedandex(props) {
                 })
     }, []);
     return (
-            <div className={"contentContainer"}>
+            <div className={"itemDescription"}>
                 <div>
+                    <h1 className={""} id={0}>{name}</h1>
                     <div id={"descriptionPedandex"}>
                     </div>
                 </div>
