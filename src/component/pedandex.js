322ca19b428a9ca838630183c771456b7ddaadd9
pierre schuvey <chromatykretro@gmail.com>
2024-06-24 18:55:17 +0200

    fix

--- src/component/pedandex.js
+++ src/component/pedandex.js
@@ -24,7 +24,12 @@ function Pedandex(props) {
         });
     }, []);
     const handleSubmit = (event) => {
-        console.log( words.findIndex(obj => obj === inputRef.current.value))
+        words.map((val, key) => {
+            if(val == inputRef.current.value){
+                console.log(number);
+            }
+        })
+        console.log(words.find((element) => element == inputRef.current.value));
         event.preventDefault();
     };
     return (
