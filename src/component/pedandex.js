7239f7c814ab3b70cb06acb076acfdfada2e1ef7
pierre schuvey <chromatykretro@gmail.com>
2024-06-26 16:46:03 +0200

    fix

--- src/component/pedandex.js
+++ src/component/pedandex.js
@@ -34,81 +34,79 @@ function Pedandex(props) {
             .then(function(response){
                 setDailyGame(response.data[0])
                 setName(response.data[0].name);
+                document.getElementById("padandexName").innerText = name.replace(/[^.]/g,'x');;
+                const description = dailyGame.description;
+                var id = 0;
+                var div = document.getElementById("textToGuess");
+                const correction = {
+                    ",": " , ",
+                    "?": " ? ",
+                    ":": " : ",
+                    "'": " ' ",
+                    "(": " ( ",
+                    ")": " ) ",
+                    "/": " / ",
+                    ".": " ."
+                };
+                Axios.get("/api/getMyTokens/"+pseudo)
+                    .then(function(response){
+                        if(response.data.length > 0){
+                            setTokens(response.data[0].token)
+                        }
+                        Axios.get("/api/getPedandexWin")
+                            .then(function(response){
+                                setLeaderBoard(response.data)
+                                if(response.data.find((uc) => uc.pseudo === pseudo && uc.day === dailyGame.day)){
+                                    setCanPlay(false)
+                                    setTriesWin(response.data.find((uc) => uc.pseudo === pseudo && uc.day === dailyGame.day).tries)
+                                    description.split(" ").forEach(word => {
+                                        setWords(words => [...words,word]);
+                                        const element = document.createElement("span");
+                                        element.setAttribute("id", id);
+                                        element.innerText = word.trim();
+                                        element.style.background = 'none';
+                                        element.style.marginRight = '0';
+                                        document.getElementById("winContentId").style.display = 'block'
+                                        document.getElementById("padandexName").innerText = name
+                                        document.getElementById("padandexName").style.background = 'none'
+                                        element.setAttribute("class", "itemDescription");
+                                        id++;
+                                        div.appendChild(element);
+                                    });
+                                }else{
+                                    setCanPlay(true)
+                                    description.replace(/,|\?|\/|\\|\:|\(|\)|\'|\./g, matched => correction[matched]).split(" ").forEach(word => {
+                                        const correction2 = {
+                                            " , ": ", ",
+                                            " ? ": " ?",
+                                            " : ": " : ",
+                                            " ' ": "'",
+                                            " / ": "/",
+                                            ".": ". "
+                                        };
+                                        setWords(words => [...words,word]);
+                                        const element = document.createElement("span");
+                                        element.setAttribute("id", id);
+                                        if(word === "'" || word ==="." || word ==="," || word ==="?" || word ===":" || word ==="(" || word ===")" || word ==="/"){
+                                            element.innerText = word.trim();
+                                            element.style.background = 'none';
+                                            element.style.marginRight = '0';
+                                        }else if(word === ""){
+                                            element.style.display = "none";
+                                        }
+                                        else{
+                                            element.innerText = word.replace(/[^.]/g,'x');
+                                        }
+                                        element.setAttribute("class", "itemDescription");
+                                        id++;
+                                        div.appendChild(element);
+                                    });
+                                }
+                            })
+                    })
             })
     }, []);
 
-    useEffect(() => {
-        document.getElementById("padandexName").innerText = name.replace(/[^.]/g,'x');;
-        const description = dailyGame.description;
-        var id = 0;
-        var div = document.getElementById("textToGuess");
-        const correction = {
-            ",": " , ",
-            "?": " ? ",
-            ":": " : ",
-            "'": " ' ",
-            "(": " ( ",
-            ")": " ) ",
-            "/": " / ",
-            ".": " ."
-        };
-        Axios.get("/api/getMyTokens/"+pseudo)
-            .then(function(response){
-                if(response.data.length > 0){
-                    setTokens(response.data[0].token)
-                }
-                Axios.get("/api/getPedandexWin")
-                    .then(function(response){
-                        setLeaderBoard(response.data)
-                        if(response.data.find((uc) => uc.pseudo === pseudo && uc.day === dailyGame.day)){
-                            setCanPlay(false)
-                            setTriesWin(response.data.find((uc) => uc.pseudo === pseudo && uc.day === dailyGame.day).tries)
-                            description.split(" ").forEach(word => {
-                                setWords(words => [...words,word]);
-                                const element = document.createElement("span");
-                                element.setAttribute("id", id);
-                                element.innerText = word.trim();
-                                element.style.background = 'none';
-                                element.style.marginRight = '0';
-                                document.getElementById("winContentId").style.display = 'block'
-                                document.getElementById("padandexName").innerText = name
-                                document.getElementById("padandexName").style.background = 'none'
-                                element.setAttribute("class", "itemDescription");
-                                id++;
-                                div.appendChild(element);
-                            });
-                        }else{
-                            setCanPlay(true)
-                            description.replace(/,|\?|\/|\\|\:|\(|\)|\'|\./g, matched => correction[matched]).split(" ").forEach(word => {
-                                const correction2 = {
-                                    " , ": ", ",
-                                    " ? ": " ?",
-                                    " : ": " : ",
-                                    " ' ": "'",
-                                    " / ": "/",
-                                    ".": ". "
-                                };
-                                setWords(words => [...words,word]);
-                                const element = document.createElement("span");
-                                element.setAttribute("id", id);
-                                if(word === "'" || word ==="." || word ==="," || word ==="?" || word ===":" || word ==="(" || word ===")" || word ==="/"){
-                                    element.innerText = word.trim();
-                                    element.style.background = 'none';
-                                    element.style.marginRight = '0';
-                                }else if(word === ""){
-                                    element.style.display = "none";
-                                }
-                                else{
-                                    element.innerText = word.replace(/[^.]/g,'x');
-                                }
-                                element.setAttribute("class", "itemDescription");
-                                id++;
-                                div.appendChild(element);
-                            });
-                        }
-                    })
-            })
-    }, [dailyGame.length > 0]);
     const handleSubmit = (event) => {
         setTries(tries + 1);
         words.map((val, key) => {
