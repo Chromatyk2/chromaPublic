d48ad3cbd1a0eececbd8cd1cc1c1b01ac7371eeb
pierre schuvey <chromatykretro@gmail.com>
2024-06-26 13:23:01 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import OtherDex from "./component/otherDex";
 import RandomProfil from "./component/randomProfil";
 import OtherProfil from "./component/otherProfil";
 import Pedandex from "./component/pedandex";
+import StartPedandex from "./component/startPedandex";
 function App(props) {
   const [cookies, setCookie] = useCookies();
   if(Object.keys(cookies).length == 0) {
@@ -61,6 +62,7 @@ function App(props) {
           <Route path="/allProfils" element={<AllProfils cookies={cookies} />} />
           <Route path="/29ct92B3ZrvxGSrp" element={<RandomProfil cookies={cookies} />} />
           <Route path="/pedandex" element={<Pedandex cookies={cookies} />} />
+          <Route path="/VXJ325De2ee8ah" element={<StartPedandex cookies={cookies} />} />
         </Routes>
           <Footer cookies={cookies} />
       </BrowserRouter>
