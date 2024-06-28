dc05d9706d685f9bbb6295489482674c3eeba613
pierre schuvey <chromatykretro@gmail.com>
2024-06-28 17:19:51 +0200

    fix

--- src/component/specificPedandex.js
+++ src/component/specificPedandex.js
@@ -420,7 +420,7 @@ function SpecificPedandex(props) {
 
             <Modal className={"modalLeaderBoard"} isOpen={modalIsOpenHistory} onRequestClose={closeModalHistory} contentLabel="Example Modal">
                 <>
-                    <p style={{textAlign:"center"}}>Historique des parties</p>
+                    <p style={{textAlign:"center"}}>Historique des partie</p>
                     <table style={{display:"flex",justifyContent:"center"}}>
                         <tbody>
                         {myHistory.length > 0 &&
