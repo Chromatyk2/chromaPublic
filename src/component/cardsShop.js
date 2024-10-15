2e97315aee9a983af673ce63384aa2a2c09f6d64
pierre schuvey <chromatykretro@gmail.com>
2024-10-15 12:09:01 +0200

    fix

--- src/component/cardsShop.js
+++ src/component/cardsShop.js
@@ -53,7 +53,7 @@ function CardsShop(props) {
                         if(response.data[0].canOpen == 1){
                             setCanOpenLive(response.data[0].canOpen)
                         }else{
-                            if(Date.now() - 3600 > new Date(moment(response.data[0].lastOpening).format('YYYY-MM-DD h:mm:ss')).getTime()){
+                            if(Date.now() - new Date(moment(response.data[0].lastOpening).format('YYYY-MM-DD h:mm:ss')).getTime() > 3600){
                                 setCanOpenLive(1)
                             }else{
                                 setCanOpenLive(0)
