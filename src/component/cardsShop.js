ede92a645595e749b69f342aa824eb07a308ab10
pierre schuvey <chromatykretro@gmail.com>
2024-10-15 12:41:41 +0200

    fix

--- src/component/cardsShop.js
+++ src/component/cardsShop.js
@@ -51,7 +51,7 @@ function CardsShop(props) {
                 Axios.get("/api/getProfil/"+props.user)
                     .then(function(response){
                         console.log(Date.now());
-                        console.log(new Date(moment(response.data[0].lastOpening).format('YYYY-MM-DD h:mm:ss')).getTime());
+                        console.log(new Date(moment(response.data[0].lastOpening).format('YYYY-MM-DD h:mm:ss')).getTime() + 36000);
                         console.log((Date.now() - new Date(moment(response.data[0].lastOpening).format('YYYY-MM-DD h:mm:ss')).getTime()));
                         if(response.data[0].canOpen == 1){
                             setCanOpenLive(response.data[0].canOpen)
