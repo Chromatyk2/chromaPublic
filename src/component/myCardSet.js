218948bad3a8fefb56592d2a895a633e16b38594
pierre schuvey <chromatykretro@gmail.com>
2024-10-16 12:11:15 +0200

    fix

--- src/component/myCardSet.js
+++ src/component/myCardSet.js
@@ -9,6 +9,7 @@ import ProgressBarCard from "./progressBarCard";
 import UniqueCard from "./UniqueCard.js";
 import Modal from "react-modal";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function MyCardsSet(props) {
     const [error, setError] = useState(null);
