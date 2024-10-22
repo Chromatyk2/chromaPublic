752dcc54486bb0be20d9380e499842ebdb702f0b
pierre schuvey <chromatykretro@gmail.com>
2024-10-22 20:59:43 +0200

    fix

--- src/component/test/openingCards.js
+++ src/component/test/openingCards.js
@@ -1,7 +1,7 @@
 import React,{useState, useEffect} from 'react';
 import Axios from 'axios'
 import '../../App.css'
-import token from '../token.png'
+import token from '../../token.png'
 
 function OpeningCardsTest(props) {
 
