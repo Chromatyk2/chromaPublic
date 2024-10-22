56e46ea856c4a977559e4aa999a7486d3d61545d
pierre schuvey <chromatykretro@gmail.com>
2024-10-22 14:17:25 +0200

    fix

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -44,11 +44,11 @@ function NavBar(props) {
                       <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/allProfils">Classement</Link>
                       <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/tcg/cartes">Mes cartes</Link>
                       <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/tcg/boutique">Ouverture Booster</Link>
-
-                      <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/pedandex">Pedandex</Link>
                       {pseudo == "chromatyk" &&
                                     <>
                                         {/*<Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/tcg/boosters">Mes boosters</Link>*/}
+
+                                        <Link style={{color:"red"}} onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/pedandex">Pedandex</Link>
                                         <Link style={{color:"red"}} onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/49Vs5sWVS2e7qs">Générateur</Link>
                                       {/*  <Link className="navLink" to="/pokedex">Pokedex</Link>*/}
                                       {/*  <Link className="navLink" to="/leaderboard">Classement</Link>*/}
