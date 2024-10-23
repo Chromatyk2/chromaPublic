6bc981c712ff93e017d4d29b97bb2d295dd60c4e
pierre schuvey <chromatykretro@gmail.com>
2024-10-23 15:29:12 +0200

    fix

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -318,7 +318,7 @@ function OpeningCards(props) {
                 </div>
             }
             {isToken === true &&
-                <div id={"shadowBox"}>
+                <div style={{position: "absolute"}} id={"shadowBox"}>
                     <div className={"newTokenContainer"}>
                         <p className={"rainbow rainbow_text_animated_token"}>1 Token Pokemon !!</p>
                         <p className="rainbow rainbow_text_animated" style={{fontSize: "10px",marginTop: "-15px"}}>Utilisez le sur la page profil</p>
