71494928b2cea2cd8e40e2cab6890b94143d9e1a
pierre schuvey <chromatykretro@gmail.com>
2024-11-05 22:35:11 +0100

    fix

--- src/component/spawnPokemonToken.js
+++ src/component/spawnPokemonToken.js
@@ -15,7 +15,7 @@ function SpawnPokemonToken(props) {
     const [getToken, setGetToken] = useState(false);
     setTimeout(() => {
         props.change();
-    }, 2500);
+    }, 3500);
     useEffect(() => {
         var tokenBonus = Math.floor(Math.random() * 10);
         if(tokenBonus == 0){
