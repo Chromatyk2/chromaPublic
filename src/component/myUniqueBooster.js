e4a74de4b1c31c8713c492f76b8a9087a891c00f
pierre schuvey <chromatykretro@gmail.com>
2024-11-20 16:45:24 +0100

    fix

--- src/component/myUniqueBooster.js
+++ src/component/myUniqueBooster.js
@@ -9,15 +9,11 @@ import MyCardsSet from './myCardSet.js';
 import SmallProgressBarCard from "./smallProgressBar";
 
 function MyUniqueBooster(props) {
-
-    const [booster, setBooster] = useState(props.boosterList.find((uc) => uc.nameGuru == props.nbCard.booster));
     function displayPage(e) {
         var page = e.target.value;
         var nbCard = e.target.getAttribute("nbCard")
-        var guruName = e.target.getAttribute("guruName")
-        props.change(page,nbCard, guruName);
+        props.change(page,nbCard);
     }
-    console.log(booster.nameGuru)
     return (
                             <div style={{filter: props.maxBooster == props.nbCard.nbCard && 'drop-shadow(0px 0px 6px orange) drop-shadow(0px 0px 47px yellow)'}} className="uniqueMyCardContainer">
                                 {props.maxBooster == props.nbCard.nbCard &&
@@ -27,7 +23,7 @@ function MyUniqueBooster(props) {
                                     <img className="fit-picture" src={"/Boosters/" +props.nbCard.booster + ".png"} alt="Grapefruit slice atop a pile of other slices"/>
                                 </div>
                                 <SmallProgressBarCard getNb={props.nbCard.nbCard} item={props.maxBooster}/>
-                                <button guruName={booster.nameGuru} value={booster.name} onClick={displayPage} className="guessTradeButton">Voir toute mes cartes</button>
+                                <button value={props.nbCard.booster} onClick={displayPage} className="guessTradeButton">Voir toute mes cartes</button>
                             </div>
     )
 }
