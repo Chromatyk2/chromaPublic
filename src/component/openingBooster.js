dd76bda29f01b7440915f74f799254d9002cb6a5
pierre schuvey <chromatykretro@gmail.com>
2024-11-20 21:11:29 +0100

    fix

--- src/component/openingBooster.js
+++ src/component/openingBooster.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState, useEffect, Suspense} from 'react';
 import { useParams } from 'react-router-dom'
 import ReactPaginate from 'react-paginate';
 import Axios from 'axios'
