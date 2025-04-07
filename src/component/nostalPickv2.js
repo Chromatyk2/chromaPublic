3225af96f436e2c77732ee7258cf8569c625c8f6
pierre schuvey <chromatykretro@gmail.com>
2025-04-07 13:23:15 +0200

    fix

--- src/component/nostalPickv2.js
+++ src/component/nostalPickv2.js
@@ -58,7 +58,7 @@ function NostalPickV2(props) {
                         )
                     })}
             </div>
-            <Modal isOpen={modalIsOpen} style={customStyles.content} contentLabel="Example Modal">
+            <Modal isOpen={modalIsOpen} style={customStyles.content} contentLabel="Example Modal" id={"modalBoxNostal"}>
                 <UniqueBoxV2 consolePicked = {pickConsole} change={handleState} />
             </Modal>
         </>
