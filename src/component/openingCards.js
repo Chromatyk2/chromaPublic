73274f84c396b1bebcd0fa02af525c670b87dbd5
pierre schuvey <chromatykretro@gmail.com>
2025-05-07 15:17:47 +0200

    fix

--- src/component/openingCards.js
+++ src/component/openingCards.js
@@ -100,7 +100,7 @@ function OpeningCards(props) {
             }else{
                 var rarity = commonRarities[Math.floor(Math.random() * commonRarities.length)]
             }
-            fetch('https://api.pokemontcg.io/v2/cards?q=set.id:'+props.idBooster.replace(".","pt").replace("0","")+'&rarity:"'+rarity.rarity+'"')
+            fetch('https://api.pokemontcg.io/v2/cards?q=set.id:'+props.idBooster.replace(".","pt").replace("0","")+' rarity:"'+rarity.rarity+'"')
                 .then(res => res.json())
                 .then(
                     (result) => {
