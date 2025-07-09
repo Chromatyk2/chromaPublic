44d6377db898300eb6c3c2a4295621a856bcd14a
pierre schuvey <chromatykretro@gmail.com>
2025-07-09 10:31:09 +0200

    fix

--- src/component/otherProfil.js
+++ src/component/otherProfil.js
@@ -321,8 +321,8 @@ function Profil(props) {
                                 {pourcent > 0 &&
                                     <div className="anchorTooltip"
                                          data-tooltip-content={pourcent == 100 ? "100% du Pokedex Complété" : pourcent >= 80 ? "80% du Pokedex Complété" : pourcent >= 60 ? "60% du Pokedex Complété" : pourcent >= 40 ? "40% du Pokedex Complété" : pourcent >= 20 ? "20% du Pokedex Complété" : "Au moins 1 Pokémon capturé"}
-                                         style={{width: "120px", height: "95px"}}>
-                                        <img style={{width: "60px"}}
+                                         style={{width: "120px"}}>
+                                        <img style={{width: "80%"}}
                                              src={pourcent == 100 ? Lv6 : pourcent >= 80 ? Lv5 : pourcent >= 60 ? Lv4 : pourcent >= 40 ? Lv3 : pourcent >= 20 ? Lv2 : Lv1}/>
                                     </div>
                                 }
