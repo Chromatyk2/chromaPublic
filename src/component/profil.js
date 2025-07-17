df11d09d3fc1dadc4fbee02180723a8a299436fb
pierre schuvey <chromatykretro@gmail.com>
2025-07-17 15:51:38 +0200

    fix

--- src/component/profil.js
+++ src/component/profil.js
@@ -506,7 +506,6 @@ function Profil(props) {
                                                                     })
                                                             })
                                                     }
-                                                }
                                             })
                                     })
                             })
