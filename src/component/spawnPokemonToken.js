fda8b162349dc738336adbc83ff8cbbdb6962c5b
pierre schuvey <chromatykretro@gmail.com>
2025-07-24 15:40:49 +0200

    fix

--- src/component/spawnPokemonToken.js
+++ src/component/spawnPokemonToken.js
@@ -282,7 +282,7 @@ function SpawnPokemonToken(props) {
                             </div>
                             <div className="mon">
                                 {getToken === true && <div className="fav-token"><img src={card} style={{width:"50px"}} /><p className={"moreToken"}>+1</p></div> }
-                                {getBadge === true && <div className="fav-token"><img src={"/Ribbon/Pokemon_("+getPkmId+")"} style={{width:"50px"}} /></div> }
+                                {getBadge === true && <div className="fav-token"><img src={"/Ribbon/Pokemon_("+getPkmId+").png"} style={{width:"50px"}} /></div> }
                                 {shiny === true &&
                                     <div className="fav">
                                         <svg className="fav-star" viewBox="0 0 114 110">
