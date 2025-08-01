967bda70d0634cfc184dea00a28c2120bc134726
pierre schuvey <chromatykretro@gmail.com>
2025-08-01 16:06:35 +0200

    fix

--- src/component/spawnPokemon.js
+++ src/component/spawnPokemon.js
@@ -43,8 +43,7 @@ function SpawnPokemon(props) {
                             const queryParameters = new URLSearchParams(window.location.search)
                             const isShiny = Math.floor((Math.random() * 100) + 1);
                             const name = result.names.find((element) => element.language.name == "fr").name;
-                            // const getBadge = Math.floor((Math.random() * 30) + 1);
-                            const getBadge = 16;
+                            const getBadge = Math.floor((Math.random() * 30) + 1);
                             const getRareBadge = Math.floor((Math.random() * 4096) + 1);
                             setGetPkmId(result.id);
                             switch (result.is_legendary){
