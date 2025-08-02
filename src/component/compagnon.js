5c1ba1d021fc086e5537ddfecd2e41e95d73d70d
pierre schuvey <chromatykretro@gmail.com>
2025-08-02 05:45:01 +0200

    fix

--- src/component/compagnon.js
+++ src/component/compagnon.js
@@ -73,8 +73,8 @@ function Compagnon(props) {
                 const compagnonList = response.data;
                 console.log(compagnonList)
                 console.log(e)
-                console.log(compagnonList.find((item) => item.pokemon === e))
-                console.log(compagnonList.filter((item) => item.pokemon === e))
+                console.log(compagnonList.find((item) => item.pokemon == e))
+                console.log(compagnonList.filter((item) => item.pokemon == e))
                 if (compagnonList.length == 0 || typeof compagnonList.find((item) => item.pokemon === e) === "undefined") {
                     Axios.post('/api/updateCompagnon',
                         {
