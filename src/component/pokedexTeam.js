ad1518a586931cfddb25f0635c4cf6c2d6b8efef
pierre schuvey <chromatykretro@gmail.com>
2025-08-03 03:35:43 +0200

    fix

--- src/component/pokedexTeam.js
+++ src/component/pokedexTeam.js
@@ -5,13 +5,23 @@ import '../App.css'
 import PkmListTeam from './pkmListTeam.js'
 
 function PokedexTeam(props) {
+    const [compagnonList,setCompagnonList] = useState(null);
     function handleState(e,f) {
         props.change(e,f);
     }
+
+    useEffect(() => {
+
+        Axios
+            .get("/api/getCompagnonList/" + pseudo)
+            .then(function (response) {
+                setCompagnonList(response.data);
+            })
+    }, [])
     return (
         <>
             <div  style={{paddingTop:"15px",minHeight:"100%"}} className={"contentContainer"}>
-                <PkmListTeam compagnonList={props.compagnonList} change={(e,f) => handleState(e,f)} pkmToUpdate={props.pkmToUpdate} list={props.list}/>
+                <PkmListTeam compagnonList={compagnonList} change={(e,f) => handleState(e,f)} pkmToUpdate={props.pkmToUpdate} list={props.list}/>
             </div>
         </>
     )
