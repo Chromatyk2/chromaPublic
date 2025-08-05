07abedf52b845db6f06768c66e227b024807151f
pierre schuvey <chromatykretro@gmail.com>
2025-08-05 15:59:36 +0200

    fix

--- src/component/myCardSet.js
+++ src/component/myCardSet.js
@@ -301,18 +301,6 @@ function MyCardsSet(props) {
     const handleRarity = event => {
         setFilterRarity(event.target.value);
     };
-    function filterEmptyCard(e) {
-        setMyCards([])
-        Axios
-            .get("/api/getMyCardsBySetAndStade/"+props.user+"/"+props.idBooster)
-            .then(function(response) {
-                response.data.filter((uc) => uc.stade != e.target.value).map((val, key) => {
-                    if(!myCardsId.find((uc) => uc.card = val.card)){
-                        setMyCards(myCardsId => [...myCardsId, val.card]);
-                    }
-                })
-            })
-    }
     return (
         <>
             {isLoaded === false ?
@@ -423,33 +411,6 @@ function MyCardsSet(props) {
                     </Modal>
                     <ProgressBarCard refresh={refresh} global={false} user={props.user} booster={props.idBooster} getNb={myCards.length}
                                      item={items.length}/>
-                    <div style={{width:"100%", display:"flex", justifyContent:"center", gap:"10px", marginTop:"-50px"}}>
-                        <button style={{width: "70px",
-                            height: "70px",
-                            backgroundColor: "transparent",
-                            border: "none",
-                            backgroundSize: "contain",
-                            backgroundPosition: "center",
-                            backgroundImage: 'url("/images/stade_1.png")'}} value={1} onClick={filterEmptyCard}></button>
-                        <button style={{width: "70px",
-                            height: "70px",
-                            backgroundColor: "transparent",
-                            border: "none",
-                            backgroundSize: "contain",
-                            backgroundPosition: "center",backgroundImage: 'url("/images/stade_2.png")'}} value={2} onClick={filterEmptyCard}></button>
-                        <button style={{width: "70px",
-                            height: "70px",
-                            backgroundColor: "transparent",
-                            border: "none",
-                            backgroundSize: "contain",
-                            backgroundPosition: "center",backgroundImage: 'url("/images/stade_3.png")'}} value={3} onClick={filterEmptyCard}></button>
-                        <button style={{width: "70px",
-                            height: "70px",
-                            backgroundColor: "transparent",
-                            border: "none",
-                            backgroundSize: "contain",
-                            backgroundPosition: "center",backgroundImage: 'url("/images/stade_4.png")'}} value={4} onClick={filterEmptyCard}></button>
-                    </div>
                     <div style={{
                         color: "white",
                         display: "flex",
