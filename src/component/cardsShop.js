890a9431e44d06327dc8a1d085883cb20bcf6e18
pierre schuvey <chromatykretro@gmail.com>
2025-08-08 03:59:45 +0200

    fix

--- src/component/cardsShop.js
+++ src/component/cardsShop.js
@@ -436,7 +436,8 @@ function CardsShop(props) {
                                         position:"relative",
                                         gap:'5px',
                                         backgroundRepeat:'no-repeat',
-                                        backgroundPositionY: "-15px"
+                                        backgroundPositionY: "-15px",
+                                        filter: "drop-shadow(0px 0px 6px black)";
                                     }}>
                                         <div className={"buttonsBooster"}>
                                             <p style={{
