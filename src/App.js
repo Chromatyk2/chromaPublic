cc611f8037142c920747800c53d069feb484dbd2
pierre schuvey <chromatykretro@gmail.com>
2025-08-09 15:27:31 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
       <>
         {typeof cookies.user !== "undefined" &&
             cookies.user.data[0].login &&
-              <div className={"contentContainer"}>
+              <div style={{backgroundColor: location.href.startsWith('https://chromatyk.fr/29ct92B3Zrvx') && 'transparent'}} className={"contentContainer"}>
                 <BrowserRouter>
                   <NavBar cookies={cookies}/>
                   <OnStream/>
