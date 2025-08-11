f661c5e3b2e65675e00448e13807cbdf4c007b02
pierre schuvey <chromatykretro@gmail.com>
2025-08-11 03:12:54 +0200

    fix

--- src/component/cardsShop.js
+++ src/component/cardsShop.js
@@ -473,7 +473,7 @@ function CardsShop(props) {
                                             allBadges.filter((uc) => uc.booster == val.name).sort((a, b) => b.stade - a.stade)[0].stade == 4 ?
                                             "linear-gradient(111deg,rgba(255, 0, 0, 1) 0%,rgba(255, 154, 0, 1) 10%,rgba(208, 222, 33, 1) 20%,rgba(79, 220, 74, 1) 30%,rgba(63, 218, 216, 1) 40%,rgba(47, 201, 226, 1) 50%,rgba(28, 127, 238, 1) 60%,rgba(95, 21, 242, 1) 70%,rgba(186, 12, 248, 1) 80%,rgba(251, 7, 217, 1) 90%,rgba(255, 0, 0, 1) 100%)"
                                             : allBadges.filter((uc) => uc.booster == val.name).sort((a, b) => b.stade - a.stade)[0].stade == 3 ? "rgba(255, 226, 0,0.5)"
-                                                : allBadges.filter((uc) => uc.booster == val.name).sort((a, b) => b.stade - a.stade)[0].stade == 2 ? "rgba(27, 189, 217,0.5)"
+                                                : allBadges.filter((uc) => uc.booster == val.name).sort((a, b) => b.stade - a.stade)[0].stade == 2 ? "rgba(0, 255, 243,0.7)"
                                                     : allBadges.filter((uc) => uc.booster == val.name).sort((a, b) => b.stade - a.stade)[0].stade == 1 ? "rgba(52, 139, 72, 0.5)"
                                                         : "rgba(165, 156, 156, 0.6)", padding: "15px", borderRadius: "10px", position:"relative"}}>
                                         {allBadges.find((uc) => uc.booster == val.name) &&
