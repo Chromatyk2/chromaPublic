41f7aa5a0b8ae747514120e7f1fdc5bcab9ba0c0
pierre schuvey <chromatykretro@gmail.com>
2025-08-14 19:30:48 +0200

    fix

--- src/component/paginateTeam.js
+++ src/component/paginateTeam.js
@@ -36,6 +36,7 @@ function Items(props) {
             props.change(id, shiny);
         }
     }
+    console.log(formList)
     return (
         <>
             <div className="pokemonGlobalContainer">
@@ -61,7 +62,7 @@ function Items(props) {
                                         backgroundSize:"80px"
 
                                     }}
-                                    className={compagnonList.filter((item) => item.pokemon == formList.filter((item) => item.form_id == val.pkmId).pkm_id && item.level == 100 && item.shiny == val.shiny).length > 0 ? "maxLevelFrame" : ""}>
+                                    className={formList.filter((item) => item.form_id == val.pkmId).length > 0 && compagnonList.filter((item) => item.pokemon == formList.filter((item) => item.form_id == val.pkmId)[0].pkm_id && item.level == 100 && item.shiny == val.shiny).length > 0 ? "maxLevelFrame" : ""}>
                                 </button>
                             </>
                         )
