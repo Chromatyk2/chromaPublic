6b868f8e51cfa3ad58245395cfb2f2b7525db847
pierre schuvey <chromatykretro@gmail.com>
2025-08-15 05:26:33 +0200

    fix

--- src/component/pkmList.js
+++ src/component/pkmList.js
@@ -4,22 +4,26 @@ import Axios from 'axios'
 import Pagination from './paginate.js';
 import '../App.css'
 import {Link} from "react-router-dom";
+import {useCookies} from "react-cookie";
 function PkmList(props) {
   const pkmList = props.list;
   const shinys = pkmList.filter(item => item.shiny == 1);
   const nbShiny = shinys.length;
   const nbTotal = pkmList.length;
+  const [cookies, setCookie] = useCookies();
     return (
         <>
-            <div className={"linkList"}>
-                <Link style={{width: "50px"}} className="navLink linkFromNav" to={"/pokedex/" + props.user}><img
-                    style={{width: "100%"}} src={"/images/pokedex.png"}/></Link>
-                <Link style={{width: "50px"}} className="navLink linkFromNav" to={"/profil/" + props.user}><img
-                    style={{width: "100%"}} src={"/images/profil.png"}/></Link>
-                <Link style={{width: "50px"}} className="navLink linkFromNav"
-                      to={"/tcg/cartes/" + props.user}><img
-                    style={{width: "100%"}} src={"/images/card.png"}/></Link>
-            </div>
+            {cookies.user.data[0].login != props.user &&
+                    <div className={"linkList"}>
+                        <Link style={{width: "50px"}} className="navLink linkFromNav" to={"/pokedex/" + props.user}><img
+                        style={{width: "100%"}} src={"/images/pokedex.png"}/></Link>
+                        <Link style={{width: "50px"}} className="navLink linkFromNav" to={"/profil/" + props.user}><img
+                        style={{width: "100%"}} src={"/images/profil.png"}/></Link>
+                        <Link style={{width: "50px"}} className="navLink linkFromNav"
+                        to={"/tcg/cartes/" + props.user}><img
+                        style={{width: "100%"}} src={"/images/card.png"}/></Link>
+                    </div>
+            }
             <div className="stats">
                 <p className="labelStats">Shiny<br/><span className="valueStats">{nbShiny}</span></p>
                 <p className="labelStats">Total<br/><span className="valueStats">{nbTotal}</span></p>
