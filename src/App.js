1d80cbec3393888e340dbc32e3d73d24a7044f6f
pierre schuvey <chromatykretro@gmail.com>
2025-09-01 20:39:47 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import { useCookies } from 'react-cookie';
 import Axios from "axios";
@@ -31,7 +31,46 @@ function App() {
   if(typeof cookies.user === "undefined") {
     return <Login />
   }
-  console.log(cookies.user.data[0])
+  useEffect(() => {
+    if(typeof cookies.user !== "undefined"){
+      Axios.post('/api/updateIdProfil',
+          {
+            user:cookies.user.data[0].login,
+            idUser:cookies.user.data[0].id
+          }
+      )
+      Axios.post('/api/updateIdBadges',
+          {
+            user:cookies.user.data[0].login,
+            idUser:cookies.user.data[0].id
+          }
+      )
+      Axios.post('/api/updateIdCaptures',
+          {
+            user:cookies.user.data[0].login,
+            idUser:cookies.user.data[0].id
+          }
+      )
+      Axios.post('/api/updateIdCards',
+          {
+            user:cookies.user.data[0].login,
+            idUser:cookies.user.data[0].id
+          }
+      )
+      Axios.post('/api/updateIdCompagnon',
+          {
+            user:cookies.user.data[0].login,
+            idUser:cookies.user.data[0].id
+          }
+      )
+      Axios.post('/api/updateIdSkin',
+          {
+            user:cookies.user.data[0].login,
+            idUser:cookies.user.data[0].id
+          }
+      )
+    }
+  }, [cookies]);
   return(
       <>
         {typeof cookies.user !== "undefined" &&
