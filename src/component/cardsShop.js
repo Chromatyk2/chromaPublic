a096be4400b0ee40c85cb329deb1ff963f5b9d9d
pierre schuvey <chromatykretro@gmail.com>
2025-09-06 19:12:36 +0200

    fix

--- src/component/cardsShop.js
+++ src/component/cardsShop.js
@@ -361,14 +361,13 @@ function CardsShop(props) {
                 console.log(response)
             })
     }
-    console.log(canOpenLive)
     return (
         <>
             <Modal overlayClassName={"overlayModalToken"} className={"modalTokenProfil"} isOpen={modalIsOpenBadge} onRequestClose={closeModal} style={customStyles.modal} contentLabel="Example Modal">
                 <p style={{textAlign:"center", fontSize:"40px", marginTop:"-100px"}}>Félicitations !!! </p>
                 <img style={{marginBottom:"30px"}} className={"badgeToWin"} src={"/Ribbon/"+boosterToDisplay+"_"+badgeToWinStade+".png"}/>
                 <p style={{textAlign:"center", fontStyle:"20px"}}>Set rempli à 100% {badgeToWinStade != 0 && "avec les cartes de rareté "+ badgeToWinStade} !!</p>
-                <button style={{display:"block",margin:"auto"}} className={"filterButton"}  onClick={closeModalBadge(boosterToDisplay)}>Cool !</button>
+                <button style={{display:"block",margin:"auto"}} className={"filterButton"}  onClick={(boosterToDisplay) => closeModalBadge(boosterToDisplay)}>Cool !</button>
             </Modal>
             {onOpen === true ?
                 <OpeningBooster change={(e,f) => handleState(e,f)} idBooster={boosterId} user={props.user} boosterGuru={nameGuru} block={block} idUser={props.idUser}/>
