4fcf8d858b2b87920b8da4918678be72bdea8975
pierre schuvey <chromatykretro@gmail.com>
2025-10-29 04:05:12 +0100

    fix

--- src/component/Shinydex.js
+++ src/component/Shinydex.js
@@ -6,28 +6,27 @@ import {Link} from "react-router-dom";
 import Axios from "axios";
 
 function Shinydex() {
-    return (
 
-        <Navbar expanded={expanded} bg="light" expand="lg">
-            <Container fluid>
-                <Navbar.Brand><a href={"https://twitch.tv/chromatyk"}><img src={"/logo.png"} /></a></Navbar.Brand>
-                <Navbar.Toggle
-                    aria-controls="navbarScroll"
-                    onClick={() => setExpanded(!expanded)}
-                />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/">Accueil</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/profil">Profil</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/compagnon">Compagnon</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/pokedex">Pokedex</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/allProfils">Communauté</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/tcg/cartes">Mes cartes</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/tcg/boutique">Ouverture Booster</Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+    const [shinydex, setShinydex] = useState(null);
+    useEffect(() => {
+        Axios
+            .get("/api/getShinydex")
+            .then(function(response) {
+                setShinydex(response.data)
+            })
+    }, []);
+    return (
+        <div className={"shinydexContainer"}>
+            {shinydex &&
+                shinydex.map((val, key) => {
+                    return(
+                        <div className={"shinydexCard"}>
+                            <div className={"shinydexName"}>#{val.pkmId} {val.pokemon}<br/>{val.surnom}</div>
+                        </>
+                    )
+                })
+            }
+        </div>
     );
 }
 
