fcfdda6b67aec489d74bcb3249ad84d3ba11c28e
pierre schuvey <chromatykretro@gmail.com>
2025-11-08 00:31:29 +0100

    fix

--- src/component/fight.js
+++ src/component/fight.js
@@ -6,13 +6,7 @@ import card from "../cards.png"
 import $ from 'jquery';
 function Fight() {
     return (
-        <div style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            margin: "auto",
-            alignItems: "center",
-            height: "100%"}}>
+        <div className={"fightContainer"}>
             <img src={"/fight.jpg"} style={{width: "100%"}}/>
         </div>
     );
