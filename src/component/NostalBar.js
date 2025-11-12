24da7551c3dace9f467e2ccb4e8b987924dec647
pierre schuvey <chromatykretro@gmail.com>
2025-11-12 01:12:23 +0100

    fix

--- src/component/NostalBar.js
+++ src/component/NostalBar.js
@@ -8,6 +8,9 @@ import Axios from "axios";
 import OBSWebSocket, {EventSubscription} from 'obs-websocket-js';
 
 function NostalBar(props) {
+    const [JouerBonusTrois, setJouerBonusTrois] = useState(true);
+    const [JouerBonusDeux, setJouerBonusDeux] = useState(true);
+    const [JouerBonusUn, setJouerBonusUn] = useState(true);
     window.addEventListener("Hauteur", (param) => {
         var obj = JSON.parse(param.detail.data)
         if(obj.recall > 0) {
@@ -18,13 +21,28 @@ function NostalBar(props) {
             document.getElementById("bonus").style.display = "block";
             document.getElementById("soluce").style.display = "block";
             document.getElementById("saveState").style.display = "block";
+            if(JouerBonusTrois === true){
+                setJouerBonusTrois(false)
+                $('audio#rainbowWin')[0].pause();
+                $('audio#rainbowWin')[0].currentTime = 0;
+            }
         }else{
             document.getElementById("bar").style.height = obj.hauteur+"%";
-            if(obj.hauteur > 29 && obj.hauteur < 59){
+            if(obj.hauteur > 29){
                 document.getElementById("soluce").style.display = "block";
-            }else if(obj.hauteur > 59 && obj.hauteur < 99){
+                if(JouerBonusUn === true){
+                    setJouerBonusUn(false)
+                    $('audio#rainbowWin')[0].pause();
+                    $('audio#rainbowWin')[0].currentTime = 0;
+                }
+            }else if(obj.hauteur > 59){
                 document.getElementById("soluce").style.display = "block";
                 document.getElementById("saveState").style.display = "block";
+                if(JouerBonusDeux === true){
+                    setJouerBonusDeux(false)
+                    $('audio#rainbowWin')[0].pause();
+                    $('audio#rainbowWin')[0].currentTime = 0;
+                }
             }else if(obj.hauteur < 29){
                 document.getElementById("soluce").style.display = "none";
                 document.getElementById("saveState").style.display = "none";
@@ -35,17 +53,19 @@ function NostalBar(props) {
     return(
         <>
             <div id={"boxContainerId"} className="boxContainer">
-                <div style={{display: "flex",
+                <div style={{
+                    display: "flex",
                     justifyContent: "center",
                     gap: "20px",
                     position: "absolute",
                     top: "200px",
-                    left: "470px"}}>
-                    <i id={"soluce"} style={{display:"none",color:"white",marginLeft: "-6px", marginTop: "4px"}}
+                    left: "470px"
+                }}>
+                    <i id={"soluce"} style={{display: "none", color: "white", marginLeft: "-6px", marginTop: "4px"}}
                        className="fa-solid fa-pen-to-square"></i>
-                    <i id={"saveState"}  style={{display:"none",color:"white",marginLeft: "-6px", marginTop: "4px"}}
+                    <i id={"saveState"} style={{display: "none", color: "white", marginLeft: "-6px", marginTop: "4px"}}
                        className="fa-solid fa-floppy-disk"></i>
-                    <i id={"bonus"}  style={{display:"none",color:"white",marginLeft: "-6px", marginTop: "4px"}}
+                    <i id={"bonus"} style={{display: "none", color: "white", marginLeft: "-6px", marginTop: "4px"}}
                        className="fa-solid fa-pencil"></i>
                 </div>
                 <div className="barcontainer">
@@ -53,6 +73,9 @@ function NostalBar(props) {
                     </div>
                 </div>
             </div>
+            <audio id="rainbowWin">
+                <source src="sounds/RainbowWin.mp3" type="audio/mpeg"/>
+            </audio>
         </>
     )
 }
