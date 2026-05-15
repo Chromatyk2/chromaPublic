c5408cd250568e3837f90f1df48c5fd55410462e
pierre schuvey <chromatykretro@gmail.com>
2026-05-15 21:37:02 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
 import 'react-tooltip/dist/react-tooltip.css'
 import HomePage from './component/home.js';
-import NostalPickV2 from "./component/stream/nostalPickv2";
 import Mentions from "./component/Mentions";    
-import OneBox from "./component/stream/OneBox";
 function App() {
   return(
       <>
@@ -13,8 +11,6 @@ function App() {
                 <BrowserRouter>
                   <Routes>
                     <Route path="/" element={<HomePage/>}/>
-                    <Route path="/29ct92B3ZrvxGS" element={<NostalPickV2 />}/>
-                    <Route path="/29ct92B3ZrvxGSO" element={<OneBox />}/>
                     <Route path="/Mentions" element={<Mentions />}/>
                   </Routes>
                 </BrowserRouter>
