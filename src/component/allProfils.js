0d47c333cae44065e3e05ed72eefa9a02de2d520
pierre schuvey <chromatykretro@gmail.com>
2026-05-16 00:37:13 +0200

    fix

---
+++ src/component/allProfils.js
@@ -0,0 +1,25 @@
+import React,{useState, useEffect} from 'react';
+import { Cookies, useCookies } from 'react-cookie';
+import Axios from 'axios'
+import '../App.css'
+import PkmList from './pkmList.js'
+import ProfilList from "./profilList";
+import OnStream from "./onStream";
+
+function AllProfils(props) {
+    const [allProfil,setAllProfil] = useState([]);
+    useEffect(() => {
+        Axios
+            .get("/api/getAllProfil")
+            .then(function(response){
+                setAllProfil(response.data);
+            })
+    }, [])
+    return (
+        <>
+                <ProfilList list={allProfil}/>
+        </>
+    )
+}
+
+export default AllProfils
