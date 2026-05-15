003928cb269dc920bf8f83631ba980d882462bcd
pierre schuvey <chromatykretro@gmail.com>
2026-05-15 21:19:26 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import 'react-tooltip/dist/react-tooltip.css'
 import HomePage from './component/home.js';
 import LastGames from "./component/stream/lastGame.js";
-import Onstream from "./component/stream/onstream";
 import NostalPickV2 from "./component/stream/nostalPickv2";
 import LastBangerAlert from "./component/stream/lastBangerAlert";
 import Mentions from "./component/Mentions";    
@@ -17,7 +16,6 @@ function App() {
       <>
               <div style={{background: window.location.href.startsWith('https://chromatyk.fr/29ct92B3Zrvx') && 'transparent'}} className={"contentContainer"}>
                 <BrowserRouter>
-                  <Onstream/>
                   <Routes>
                     <Route path="/" element={<HomePage/>}/>
                     <Route path="/29ct92B3ZrvxGS" element={<NostalPickV2 />}/>
