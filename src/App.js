859a95eca441dc1734de472b1684c0cc15923b18
pierre schuvey <chromatykretro@gmail.com>
2026-05-15 21:16:35 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
 import 'react-tooltip/dist/react-tooltip.css'
-import HomePage from './component/Stream/home.js';
-import LastGames from "./component/Stream/lastGame.js";
-import OnStream from "./component/Stream/onStream";
-import NostalPickV2 from "./component/Stream/nostalPickv2";
-import LastBangerAlert from "./component/Stream/lastBangerAlert";
+import HomePage from './component/home.js';
+import LastGames from "./component/stream/lastGame.js";
+import Onstream from "./component/stream/onstream";
+import NostalPickV2 from "./component/stream/nostalPickv2";
+import LastBangerAlert from "./component/stream/lastBangerAlert";
 import Mentions from "./component/Mentions";    
-import SpawnPokemon from "./component/Stream/spawnPokemon";
-import RandomProfil from "./component/Stream/randomProfil";
-import Prediction from "./component/Stream/prediction";
-import OneBox from "./component/Stream/OneBox";
+import SpawnPokemon from "./component/stream/spawnPokemon";
+import RandomProfil from "./component/stream/randomProfil";
+import Prediction from "./component/stream/prediction";
+import OneBox from "./component/stream/OneBox";
 function App() {
   return(
       <>
               <div style={{background: window.location.href.startsWith('https://chromatyk.fr/29ct92B3Zrvx') && 'transparent'}} className={"contentContainer"}>
                 <BrowserRouter>
-                  <OnStream/>
+                  <Onstream/>
                   <Routes>
                     <Route path="/" element={<HomePage/>}/>
                     <Route path="/29ct92B3ZrvxGS" element={<NostalPickV2 />}/>
